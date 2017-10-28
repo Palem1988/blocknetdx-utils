19ce7bd2a7cbafc13d3939f3affdba666e21a4b1
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-28 23:39:53 +0200

    Add files via upload

--- rpc_test_cases/xbridge_rpc_sequence_test.py
+++ rpc_test_cases/xbridge_rpc_sequence_test.py
@@ -10,21 +10,17 @@ from interface import xbridge_rpc
         - VARIOUS RPC CALLS
         - THE LIST OF RPC CALLS CAN BE EASILY ADJUSTED IF NECESSARY
 """
+
 # TODO: getservicenodelist
-def test_random_sequence_valid_invalid_inputs(nb_of_runs, char_min_size, char_max_size):
+def test_random_sequence_(nb_of_runs, data_nature, char_min_size, char_max_size):
     no_param_func_list = [xbridge_rpc.get_transaction_list, xbridge_rpc.get_transaction_history_list, xbridge_rpc.get_currency_list, xbridge_rpc.getblockcount]
     txid_func_list = [xbridge_rpc.cancel_tx, xbridge_rpc.get_tx_info, xbridge_rpc.decode_raw_tx, xbridge_rpc.send_tx, xbridge_rpc.sign_tx]
     time_distribution = []
     total_elapsed_seconds = 0
     elapsed_Time = 0
-    # At each run, generate a mix of valid / invalid / out-of-bounds data
     for j in range(1, nb_of_runs):
         func_list = random.choice([no_param_func_list, txid_func_list, xbridge_rpc.create_tx, xbridge_rpc.accept_tx])
-        VALID_INVALID_PICK = random.choice(["valid", "invalid"])
-        if VALID_INVALID_PICK == "invalid":
-            xbridge_utils.generate_new_set_of_data(True)
-        else:
-            xbridge_utils.generate_new_set_of_data(False)
+        xbridge_utils.generate_new_set_of_data(data_nature)
         if func_list == xbridge_rpc.create_tx:
             picked_func = "create_tx"
             ts = time.time()
@@ -64,15 +60,12 @@ def test_random_sequence_valid_invalid_inputs(nb_of_runs, char_min_size, char_ma
 
 """
 
-def test_defined_order_sequence(nb_of_runs, invalid_data_bool):
+def test_defined_order_sequence(nb_of_runs, data_nature):
     time_distribution = []
     total_elapsed_seconds = 0
     elapsed_Time = 0
     for i in range(1, nb_of_runs):
-        if invalid_data_bool:
-            xbridge_utils.generate_new_set_of_data(True)
-        else:
-            xbridge_utils.generate_new_set_of_data(False)
+        xbridge_utils.generate_new_set_of_data(data_nature)
         print("defined order RPC sequence #%s" % str(i))
         ts = time.time()
         xbridge_rpc.create_tx(xbridge_utils.c_src_Address, xbridge_utils.c_src_Token, xbridge_utils.source_nb,
