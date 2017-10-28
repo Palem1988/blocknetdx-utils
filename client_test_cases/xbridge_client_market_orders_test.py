9d72513b9297cdd6f7166c417b74f85f042e4629
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-28 21:47:23 +0200

    Add files via upload

--- client_test_cases/xbridge_client_market_orders_test.py
+++ client_test_cases/xbridge_client_market_orders_test.py
@@ -9,41 +9,6 @@ from interface import xbridge_client
     - CANCELTX + ACCEPT_TX + CREATE_TX
 """
 
-c_src_Address = ""
-c_dest_Address = ""
-c_src_Token = ""
-c_dest_Token = ""
-source_nb = ""
-dest_nb = ""
-a_random_tx_id = ""
-a_src_Address = ""
-a_dest_Address = ""
-ca_random_tx_id = ""
-
-
-def generate_new_set_of_data():
-    c_src_Address = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-    c_dest_Address = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-    c_src_Token = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-    c_dest_Token = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-    source_nb = xbridge_utils.generate_random_number(-999999999999999999999999999999999999999999999999999999999999999,
-                                                     999999999999999999999999999999999999999999999999999999999999999)
-    dest_nb = xbridge_utils.generate_random_number(-999999999999999999999999999999999999999999999999999999999999999,
-                                                   999999999999999999999999999999999999999999999999999999999999999)
-    a_random_tx_id = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-    a_src_Address = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-    a_dest_Address = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-    ca_random_tx_id = xbridge_utils.generate_input_from_random_classes_combinations(
-        xbridge_utils.generate_random_number(0, 12000))
-
-
 
 """
     - UNORDERED SEQUENCE OF MARKET ACTIONS
@@ -54,20 +19,20 @@ def random_seq_market_actions_api_calls(nb_of_runs):
     time_distribution = []
     total_elapsed_seconds = 0
     for i in range(1, nb_of_runs):
-        generate_new_set_of_data()
+        xbridge_utils.generate_new_set_of_data()
         func_list = [xbridge_client.CHECK_CREATE_TX, xbridge_client.CHECK_ACCEPT_TX,
                                  xbridge_client.CHECK_CANCEL_TX]
         selected_func = random.choice(func_list)
         func_str = ""
         ts = time.time()
         if selected_func == xbridge_client.CHECK_CREATE_TX:
-            xbridge_client.CHECK_CREATE_TX(c_src_Address, c_src_Token, source_nb, c_dest_Address, c_dest_Token, dest_nb)
+            xbridge_client.CHECK_CREATE_TX(xbridge_utils.c_src_Address, xbridge_utils.c_src_Token, xbridge_utils.source_nb, xbridge_utils.c_dest_Address, xbridge_utils.c_dest_Token, xbridge_utils.dest_nb)
             func_str = "CHECK_CREATE_TX"
         if selected_func == xbridge_client.CHECK_ACCEPT_TX:
-            xbridge_client.CHECK_ACCEPT_TX(a_random_tx_id, a_src_Address, a_dest_Address)
+            xbridge_client.CHECK_ACCEPT_TX(xbridge_utils.a_random_tx_id, xbridge_utils.a_src_Address, xbridge_utils.a_dest_Address)
             func_str = "CHECK_ACCEPT_TX"
         if selected_func == xbridge_client.CHECK_CANCEL_TX:
-            xbridge_client.CHECK_CANCEL_TX(ca_random_tx_id)
+            xbridge_client.CHECK_CANCEL_TX(xbridge_utils.ca_random_tx_id)
             func_str = "CHECK_CANCEL_TX"
         te = time.time()
         total_elapsed_seconds += te - ts
@@ -85,17 +50,17 @@ def defined_seq_market_actions_api_calls(nb_of_runs):
     time_distribution = []
     total_elapsed_seconds = 0
     for i in range(1, nb_of_runs):
-        generate_new_set_of_data()
+        xbridge_utils.generate_new_set_of_data()
         func_str = ""
         ts = time.time()
         if i % 3 == 0:
-            xbridge_client.CHECK_CREATE_TX(c_src_Address, c_src_Token, source_nb, c_dest_Address, c_dest_Token, dest_nb)
+            xbridge_client.CHECK_CREATE_TX(xbridge_utils.c_src_Address, xbridge_utils.c_src_Token, xbridge_utils.source_nb, xbridge_utils.c_dest_Address, xbridge_utils.c_dest_Token, xbridge_utils.dest_nb)
             func_str = "CHECK_CREATE_TX"
         if i % 3 == 1:
-            xbridge_client.CHECK_ACCEPT_TX(a_random_tx_id, a_src_Address, a_dest_Address)
+            xbridge_client.CHECK_ACCEPT_TX(xbridge_utils.a_random_tx_id, xbridge_utils.a_src_Address, xbridge_utils.a_dest_Address)
             func_str = "CHECK_ACCEPT_TX"
         if i % 3 == 2:
-            xbridge_client.CHECK_CANCEL_TX(ca_random_tx_id)
+            xbridge_client.CHECK_CANCEL_TX(xbridge_utils.ca_random_tx_id)
             func_str = "CHECK_CANCEL_TX"
         te = time.time()
         total_elapsed_seconds += te - ts
