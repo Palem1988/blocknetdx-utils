233d0986c17c55c8e18059f6226e8088ce00980f
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-28 23:47:14 +0200

    Add files via upload

--- rpc_test_cases/xbridge_rpc_sequence_test.py
+++ rpc_test_cases/xbridge_rpc_sequence_test.py
@@ -51,7 +51,7 @@ def test_random_sequence_(nb_of_runs, data_nature, char_min_size, char_max_size)
         total_elapsed_seconds += elapsed_Time
         json_str = {"time": elapsed_Time, "API": str(picked_func)}
         time_distribution.append(json_str)
-    xbridge_utils.export_data("test_rpc_random_sequence_valid_invalid_inputs.xlsx", time_distribution)
+    xbridge_utils.export_data("test_rpc_random_sequence.xlsx", time_distribution)
     
     
 """                       
@@ -79,6 +79,26 @@ def test_defined_order_sequence(nb_of_runs, data_nature):
         xbridge_rpc.accept_tx(xbridge_utils.a_random_tx_id, xbridge_utils.a_src_Address, xbridge_utils.a_dest_Address)
         te = time.time()
         elapsed_Time = te - ts
-        json_str = {"time": elapsed_Time, "API": "create_tx"}
+        json_str = {"time": elapsed_Time, "API": "accept_tx"}
         time_distribution.append(json_str)
-    xbridge_utils.export_data("test_defined_order_sequence.xlsx", time_distribution)
+        # cancel_tx
+        ts = time.time()
+        xbridge_rpc.cancel_tx(xbridge_utils.ca_random_tx_id)
+        te = time.time()
+        elapsed_Time = te - ts
+        json_str = {"time": elapsed_Time, "API": "cancel_tx"}
+        time_distribution.append(json_str)
+        # get_tx_info
+        ts = time.time()
+        xbridge_rpc.get_tx_info(xbridge_utils.ca_random_tx_id)
+        te = time.time()
+        elapsed_Time = te - ts
+        json_str = {"time": elapsed_Time, "API": "get_tx_info"}
+        # get_transaction_history_list
+        ts = time.time()
+        xbridge_rpc.get_transaction_history_list()
+        te = time.time()
+        elapsed_Time = te - ts
+        json_str = {"time": elapsed_Time, "API": "get_transaction_history_list"}
+    xbridge_utils.export_data("test_rpc_defined_order_sequence.xlsx", time_distribution)
+
