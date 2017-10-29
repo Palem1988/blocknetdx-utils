9335eb70e5a868b0ea7c142243427b62176eeaaf
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-29 10:18:32 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_sequence_test.py
+++ rpc_test_cases/xbridge_rpc_sequence_test.py
@@ -11,16 +11,15 @@ from interface import xbridge_rpc
         - THE LIST OF RPC CALLS CAN BE EASILY ADJUSTED IF NECESSARY
 """
 
-# TODO: getservicenodelist
-def test_random_sequence_(nb_of_runs, data_nature, char_min_size, char_max_size):
-    no_param_func_list = [xbridge_rpc.get_transaction_list, xbridge_rpc.get_transaction_history_list, xbridge_rpc.get_currency_list, xbridge_rpc.getblockcount]
+def random_RPC_calls_sequence(nb_of_runs=1000, data_nature=xbridge_utils.RANDOM_VALID_INVALID, char_min_size=1, char_max_size=12000):
+    no_param_func_list = [xbridge_rpc.get_transaction_list, xbridge_rpc.get_transaction_history_list, xbridge_rpc.get_currency_list, xbridge_rpc.get_blockcount, xbridge_rpc.get_budget, xbridge_rpc.get_node_list]
     txid_func_list = [xbridge_rpc.cancel_tx, xbridge_rpc.get_tx_info, xbridge_rpc.decode_raw_tx, xbridge_rpc.send_tx, xbridge_rpc.sign_tx]
     time_distribution = []
     total_elapsed_seconds = 0
     elapsed_Time = 0
     for j in range(1, nb_of_runs):
         func_list = random.choice([no_param_func_list, txid_func_list, xbridge_rpc.create_tx, xbridge_rpc.accept_tx])
-        xbridge_utils.generate_new_set_of_data(data_nature)
+        xbridge_utils.generate_new_set_of_data(data_nature, char_min_size, char_max_size)
         if func_list == xbridge_rpc.create_tx:
             picked_func = "create_tx"
             ts = time.time()
@@ -60,12 +59,12 @@ def test_random_sequence_(nb_of_runs, data_nature, char_min_size, char_max_size)
 
 """
 
-def test_defined_order_sequence(nb_of_runs, data_nature):
+def defined_order_RPC_calls_sequence(nb_of_runs=1000, data_nature=xbridge_utils.RANDOM_VALID_INVALID, char_min_size=1, char_max_size=12000):
     time_distribution = []
     total_elapsed_seconds = 0
     elapsed_Time = 0
     for i in range(1, nb_of_runs):
-        xbridge_utils.generate_new_set_of_data(data_nature)
+        xbridge_utils.generate_new_set_of_data(data_nature, char_min_size, char_max_size)
         print("defined order RPC sequence #%s" % str(i))
         ts = time.time()
         xbridge_rpc.create_tx(xbridge_utils.c_src_Address, xbridge_utils.c_src_Token, xbridge_utils.source_nb,
