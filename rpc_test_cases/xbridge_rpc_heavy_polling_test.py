ad562a7ce36fafebf336756d1d6f6702d01903d6
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-29 09:31:20 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_heavy_polling_test.py
+++ rpc_test_cases/xbridge_rpc_heavy_polling_test.py
@@ -1,8 +1,8 @@
 import time
+import random
 
 from utils import xbridge_utils
 from interface import xbridge_rpc
-
 
 """
     - WE WANT TO TEST SCENARIOS IN WHICH USERS POLL CONSTANTLY THE DX TO GET INFORMATION.
@@ -12,17 +12,50 @@ from interface import xbridge_rpc
     SCENARIOS GROUPS THAT ARE TESTED HERE:
     - ORDERED SEQUENCE
     - UNORDERED RANDOM SEQUENCE
-    - SINGLE APLI SEQUENCE
+    - SINGLE RPC SEQUENCE
     
 """
 
+"""
+    - UNORDERED SEQUENCE OF RPC CALLS TO GET INFORMATION - NO MARKET ACTIONS
+    - GET_TX_LIST + GET_TRANSACTION_HISTORY + GET_TX_INFO
+"""
+
+def random_seq_polling_rpc_calls(nb_of_runs=1000, data_nature=xbridge_utils.RANDOM_VALID_INVALID):
+    time_distribution = []
+    total_elapsed_seconds = 0
+    no_param_func_list = [xbridge_rpc.get_transaction_list, xbridge_rpc.get_transaction_history_list,
+                          xbridge_rpc.get_currency_list, xbridge_rpc.get_blockcount, xbridge_rpc.get_budget,
+                          xbridge_rpc.get_node_list]
+    for i in range(1, nb_of_runs):
+        func_list = random.choice([no_param_func_list, xbridge_rpc.get_tx_info])
+        # print("#%s" % str(i))
+        func_str = ""
+        if func_list == no_param_func_list:
+            picked_func = random.choice(no_param_func_list)
+            ts = time.time()
+            picked_func()
+            te = time.time()
+            elapsed_Time = te - ts
+        if func_list == xbridge_rpc.get_tx_info:
+            xbridge_utils.generate_new_set_of_data(data_nature)
+            picked_func = "get_tx_info"
+            ts = time.time()
+            xbridge_rpc.get_tx_info()
+            te = time.time()
+            elapsed_Time = te - ts
+        total_elapsed_seconds += te - ts
+        json_str = {"time": elapsed_Time, "API": picked_func}
+        time_distribution.append(json_str)
+    xbridge_utils.export_data("random_seq_polling_rpc_calls.xlsx", time_distribution)
+
 
 """
-    - DEFINED SEQUENCE OF API CALLS TO GET INFORMATION - NO MARKET ACTIONS
+    - DEFINED SEQUENCE OF RPC CALLS TO GET INFORMATION - NO MARKET ACTIONS
     - GET_TX_LIST + GET_TRANSACTION_HISTORY
-    - ORDERED API CALL SEQUENCE
 """
-def defined_seq_get_info_api_calls(nb_of_runs):
+
+def defined_seq_polling_rpc_calls(nb_of_runs=1000):
     time_distribution = []
     total_elapsed_seconds = 0
     for i in range(1, nb_of_runs):
@@ -39,14 +72,14 @@ def defined_seq_get_info_api_calls(nb_of_runs):
         total_elapsed_seconds += te - ts
         json_str = {"time": te - ts, "API": func_str}
         time_distribution.append(json_str)
-    xbridge_utils.export_data("defined_seq_get_info_rpc_calls.xlsx", time_distribution)
+    xbridge_utils.export_data("defined_seq_polling_rpc_calls.xlsx", time_distribution)
 
 
 """                       
     ***  GET_TRANSACTION_LIST ONLY ***
 """
 
-def test_get_tx_list_load(nb_of_runs):
+def test_get_tx_list_load(nb_of_runs=1000):
     time_distribution = []
     total_elapsed_seconds = 0
     for i in range(1, nb_of_runs):
@@ -64,7 +97,7 @@ def test_get_tx_list_load(nb_of_runs):
     ***  GET_TRANSACTION_HISTORY ONLY ***
 """
 
-def test_get_tx_history_load(nb_of_runs):
+def test_get_tx_history_load(nb_of_runs=1000):
     time_distribution = []
     total_elapsed_seconds = 0
     for i in range(1, nb_of_runs):
@@ -82,7 +115,7 @@ def test_get_tx_history_load(nb_of_runs):
     ***  GET_CURRENCY_LIST ONLY ***
 """
 
-def test_get_currency_list_load(nb_of_runs):
+def test_get_currency_list_load(nb_of_runs=1000):
     time_distribution = []
     total_elapsed_seconds = 0
     for i in range(1, nb_of_runs):
