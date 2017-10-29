9e4b52459c66112f944e90ed38e67c174fb7da3b
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-29 14:57:10 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_get_tx_info_test.py
+++ rpc_test_cases/xbridge_rpc_get_tx_info_test.py
@@ -27,7 +27,7 @@ def dxGetTransactionInfo_RPC_sequence(nb_of_runs=1000, data_nature=xbridge_utils
 
 """                       ***  UNIT TESTS ***
 
-    - Assertions currently take the following form ==> self.assertIsInstance(..., dict).
+    - Assertions currently take the following form ==> self.assertIsInstance(..., list).
     More precise assertions may have to be written, when we have real data.
 
     - We test many combinations. But additional scenarios may have to be added.
@@ -67,39 +67,43 @@ class get_Tx_Info_UnitTest(unittest.TestCase):
           - We then combine those character classes.
           - Size of the input parameter is fixed.
     """
-    def invalid_get_tx_info_test_3(self):
-        # We pick from a single class at a time
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a]{64}').render()), list)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\d]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\w]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\h]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s]{64}').render()), dict)
-        # We pick from combinations of 2 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\a]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\h]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\d]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\a]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\h]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\p]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\d]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\a]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\h]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\p]{64}').render()), dict)
-        # We pick from combinations of 3 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\h]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\s]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\p]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{64}').render()), dict)
-        # We pick from combinations of 4 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\w\s]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\h]{64}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\s]{64}').render()), dict)
-        # We pick from combinations of 5 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\W\w\h\a]{64}').render()), dict)
+    def test_invalid_get_tx_info_test_3(self):
+        try:
+            # We pick from a single class at a time
+            str_1 = StringGenerator('[\a]{64}').render()
+            self.assertIsInstance(xbridge_rpc.get_tx_info(str_1), dict)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\d]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\w]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\h]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s]{64}').render()), list)
+            # We pick from combinations of 2 classes
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\a]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\h]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\d]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\a]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\h]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\p]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\d]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\a]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\h]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\p]{64}').render()), list)
+            # We pick from combinations of 3 classes
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\h]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\s]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\p]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{64}').render()), list)
+            # We pick from combinations of 4 classes
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\w\s]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\h]{64}').render()), list)
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\s]{64}').render()), list)
+            # We pick from combinations of 5 classes
+            self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\W\w\h\a]{64}').render()), list)
+        except AssertionError as e:
+            print("failed on set: %s" % str_1)
 
 
     """
@@ -107,37 +111,37 @@ class get_Tx_Info_UnitTest(unittest.TestCase):
     """
     def invalid_get_tx_info_test_4(self):
        # We pick from a single class at a time
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\d]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\w]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\h]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s]{9000:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\d]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\w]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\h]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s]{9000:11000}').render()), list)
         # We pick from combinations of 2 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\a]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\h]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\d]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\a]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\h]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\p]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\d]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\a]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\h]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\p]{9000:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\a]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\h]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\d]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\a]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\h]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\p]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\d]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\a]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\h]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\p]{9000:11000}').render()), list)
         # We pick from combinations of 3 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\h]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\s]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\p]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{9000:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\h]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\s]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\p]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{9000:11000}').render()), list)
         # We pick from combinations of 4 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\w\s]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\h]{9000:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\s]{9000:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\w\s]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\h]{9000:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\s]{9000:11000}').render()), list)
         # We pick from combinations of 5 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\W\w\h\a]{9000:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\W\w\h\a]{9000:11000}').render()), list)
 
 
     """
@@ -145,37 +149,37 @@ class get_Tx_Info_UnitTest(unittest.TestCase):
     """
     def invalid_get_tx_info_test_5(self):
         # We pick from a single class at a time
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\d]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\w]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\h]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s]{1:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\d]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\w]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\h]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s]{1:11000}').render()), list)
         # We pick from combinations of 2 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\a]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\h]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\d]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\a]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\h]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\p]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\d]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\a]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\h]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\p]{1:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\a]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\h]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\d]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\a]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\h]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\s\p]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\d]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\a]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\h]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\W\p]{1:11000}').render()), list)
         # We pick from combinations of 3 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\h]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\s]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\p]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{1:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\h]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\s]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\p]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\a\d\W]{1:11000}').render()), list)
         # We pick from combinations of 4 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\w\s]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\h]{1:11000}').render()), dict)
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\s]{1:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\w\s]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\h]{1:11000}').render()), list)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\a\s]{1:11000}').render()), list)
         # We pick from combinations of 5 classes
-        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\W\w\h\a]{1:11000}').render()), dict)
+        self.assertIsInstance(xbridge_rpc.get_tx_info(StringGenerator('[\p\d\W\w\h\a]{1:11000}').render()), list)
 
 
 def repeat_tx_info_unit_tests(nb_of_runs):
@@ -183,4 +187,7 @@ def repeat_tx_info_unit_tests(nb_of_runs):
         wasSuccessful = unittest.main(exit=False).result.wasSuccessful()
         if not wasSuccessful:
             sys.exit(1)
+
+
+unittest.main()
 
