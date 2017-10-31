bea7af0987a396e4c888a42b1e67a4bea97c5691
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-31 21:22:46 +0100

    Add files via upload

--- night_run_sequence.py
+++ night_run_sequence.py
@@ -52,7 +52,7 @@ NUMBER_OF_WANTED_RUNS = args.sequence
 UNIT_TESTS_NB_OF_RUNS = args.unittest
 """
 
-NUMBER_OF_WANTED_RUNS = 3
+NUMBER_OF_WANTED_RUNS = 0
 UNIT_TESTS_NB_OF_RUNS = 3
 
 
@@ -122,78 +122,19 @@ xbridge_rpc_market_orders_test.defined_seq_market_actions_rpc_calls(nb_of_runs=N
         *****************************************************************************************
 """
 
-
-"""
-from rpc_test_cases.xbridge_rpc_canceltx_test import *
-xbridge_rpc_canceltx_test.repeat_cancel_tx_unit_tests(4)
-# unittest.main()
-# unittest.main(defaultTest="cancel_UnitTest.test_invalid_cancel_1", exit=False)
-
-suite = unittest.TestSuite()
-suite.addTest(cancel_UnitTest('test_invalid_cancel_1'))
-print(suite)
-unittest.TextTestRunner().run(suite)
-"""
-
-
 stream = io.StringIO()
 runner = unittest.TextTestRunner(stream=stream)
 full_Test_Suite = unittest.TestSuite()
 
-for i in range(1, UNIT_TESTS_NB_OF_RUNS):
-    full_Test_Suite.addTest(xbridge_rpc_canceltx_test.cancel_UnitTest())
-    # full_Test_Suite.addTest(unittest.makeSuite(xbridge_rpc_canceltx_test.cancel_UnitTest))
-    full_Test_Suite.addTest(xbridge_rpc_get_tx_info_test.get_Tx_Info_UnitTest())
-    full_Test_Suite.addTest(xbridge_rpc_accept_tx_test.accept_Tx_Test())
-    
-testResult = runner.run(full_Test_Suite)
+unit_tests_module_strings = [xbridge_rpc_canceltx_test, xbridge_rpc_get_tx_info_test]
+suites = [unittest.TestLoader().loadTestsFromModule(modul) for modul in unit_tests_module_strings]
+test_suite = unittest.TestSuite(suites)
+testResult = unittest.TextTestRunner(verbosity=2).run(test_suite)
+# print('wasSuccessful: %s - testRuns: %s - Failures: %s - Errors: %s' % (str(testResult.wasSuccessful), str(testResult.testsRun), str(testResult.failures), str(testResult.errors)))
+xbridge_utils.logger.info('----------------------------------------------------------------------------------------------------------------------------------------------------------')
 xbridge_utils.logger.info('wasSuccessful: %s - testRuns: %s - Failures: %s - Errors: %s' % (str(testResult.wasSuccessful), str(testResult.testsRun), str(testResult.failures), str(testResult.errors)))
 
 stream.seek(0)
 """
 print('Test output\n', stream.read())
 """
-
-
-
-""" RUNS FINE
-
-xbridge_client_get_tx_info_test.test_get_tx_info_load_v1(5)
-xbridge_client_get_tx_info_test.test_get_tx_info_load_v2(5)
-xbridge_client_get_tx_info_test.test_get_tx_info_load_v3(5)
-xbridge_client_get_tx_info_test.repeat_tx_info_unit_tests(2)
-
-"""
-
-""" RUNS FINE
-
-xbridge_client_createtx_test.test_createtx_garbage_load_v1(5)
-xbridge_client_createtx_test.test_createtx_garbage_load_v2(5)
-xbridge_client_createtx_test.test_createtx_valid_load(5)
-# Unit tests
-xbridge_client_createtx_test.repeat_create_tx_unit_tests(5)
-
-"""
-
-""" RUNS FINE
-
-xbridge_client_accept_tx_test.test_accept_garbage_load_v1(5)
-xbridge_client_accept_tx_test.test_accept_garbage_load_v2(5)
-xbridge_client_accept_tx_test.test_accept_valid_load(5)
-# Unit tests
-xbridge_client_accept_tx_test.repeat_accept_tx_unit_tests(5)
-
-"""
-
-""" RUNS FINE
-
-xbridge_client_canceltx_test.test_cancel_load_v1(5)
-xbridge_client_canceltx_test.test_cancel_load_v2(5)
-xbridge_client_canceltx_test.test_cancel_load_v3(5)
-# Unit tests
-from test_cases.xbridge_client_canceltx_test import *
-# unittest.main()
-xbridge_client_canceltx_test.repeat_cancel_tx_unit_tests(3)
-
-"""
-
