b8dffa6bcdf7c7a0c68c57c2aac55c4e02c945f4
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-31 18:48:09 +0100

    Add files via upload

--- night_run_sequence.py
+++ night_run_sequence.py
@@ -1,6 +1,6 @@
 import unittest
 import io
-
+import argparse 
 from utils import xbridge_utils
 
 from test_cases import xbridge_client_get_tx_info_test
@@ -18,31 +18,52 @@ from rpc_test_cases import xbridge_rpc_heavy_polling_test
 from rpc_test_cases import xbridge_rpc_sequence_test
 
 
-"""             INSTRUCTIONS
+"""     *****************************************************************************************
+        ******************************  INSTRUCTIONS    ******************************
+        *****************************************************************************************
 
-    - All tests can be run from there.
+            - All tests can be run from there.
 
-    - Uncomment the test you want to run.
-    
-    - Adjust the number of runs you want.
+            - Uncomment the test you want to run.
+            
+            - Adjust the number of runs you want.
 
-    - Except for unit tests, all tests output an Excel file in the current directory with
-    the timing distribution.
-
-    - The higher the number of runs, the more robust the testing.
+            - Except for unit tests, all tests output an Excel file in the current directory with
+            the timing distribution.
 
 """
 
+"""            
+        *****************************************************************************************
+        ******************************  SPECIFY THE OPTIONS HERE    *****************************
+        *****************************************************************************************
+"""
+
+# SOME OTHER OPTIONS HAVE TO MODIFIED IN THE GLOB.PY MODULE
+
+parser = argparse.ArgumentParser(description='API testing')
+parser.add_argument('-s','--sequence', type=int, help='Number of sequence tests run', default=1000)
+parser.add_argument('-u','--unittest', type=int, help='Number of unit tests to run', default=1000)
+
+args = parser.parse_args()
+
+""" WILL BE UNCOMMENTED LATER
+NUMBER_OF_WANTED_RUNS = args.sequence
+UNIT_TESTS_NB_OF_RUNS = args.unittest
+"""
+
+NUMBER_OF_WANTED_RUNS = 3
+UNIT_TESTS_NB_OF_RUNS = 3
+
 
 """            
-        *************************************************************
-        ******************   RPC SEQUENCE TESTS    ******************
-        *************************************************************
+        *****************************************************************************************
+        ******************  SPECIFY HERE THE LIST RPC SEQUENCE TESTS TO RUN    ******************
+        *****************************************************************************************
 """
 
 # When data_nature is not specified, this will generate both valid and invalid data.
 
-NUMBER_OF_WANTED_RUNS = 0
 
 xbridge_rpc_sequence_test.random_RPC_calls_sequence(nb_of_runs=NUMBER_OF_WANTED_RUNS, data_nature=xbridge_utils.INVALID_DATA, char_min_size=10000, char_max_size=12000)
 xbridge_rpc_sequence_test.random_RPC_calls_sequence(nb_of_runs=NUMBER_OF_WANTED_RUNS, data_nature=xbridge_utils.VALID_DATA)
@@ -96,66 +117,43 @@ xbridge_rpc_market_orders_test.defined_seq_market_actions_rpc_calls(nb_of_runs=N
 
 
 """            
-        *******************************************************************
-        ******************   RPC UNIT TESTS LAUNCHERS    ******************
-        *******************************************************************
+        *****************************************************************************************
+        ********************  SPECIFY HERE THE LIST RPC UNITS TESTS TO RUN    *******************
+        *****************************************************************************************
 """
 
-"""                 RPC DX_CANCEL_TX
+
 """
+from rpc_test_cases.xbridge_rpc_canceltx_test import *
+xbridge_rpc_canceltx_test.repeat_cancel_tx_unit_tests(4)
+# unittest.main()
+# unittest.main(defaultTest="cancel_UnitTest.test_invalid_cancel_1", exit=False)
+
+suite = unittest.TestSuite()
+suite.addTest(cancel_UnitTest('test_invalid_cancel_1'))
+print(suite)
+unittest.TextTestRunner().run(suite)
+"""
+
 
 stream = io.StringIO()
 runner = unittest.TextTestRunner(stream=stream)
 full_Test_Suite = unittest.TestSuite()
 
-for i in range(1,3):
+for i in range(1, UNIT_TESTS_NB_OF_RUNS):
     full_Test_Suite.addTest(xbridge_rpc_canceltx_test.cancel_UnitTest())
+    # full_Test_Suite.addTest(unittest.makeSuite(xbridge_rpc_canceltx_test.cancel_UnitTest))
     full_Test_Suite.addTest(xbridge_rpc_get_tx_info_test.get_Tx_Info_UnitTest())
-
+    full_Test_Suite.addTest(xbridge_rpc_accept_tx_test.accept_Tx_Test())
+    
 testResult = runner.run(full_Test_Suite)
 xbridge_utils.logger.info('wasSuccessful: %s - testRuns: %s - Failures: %s - Errors: %s', % (str(testResult.wasSuccessful), str(testResult.testsRun), str(testResult.failures), str(testResult.errors))
 
-
-"""
 stream.seek(0)
+"""
 print('Test output\n', stream.read())
 """
 
-
-
-"""                 RPC DX_GET_TRANSACTION_INFO
-"""
-
-""" RUNS FINE
-from rpc_test_cases.xbridge_rpc_get_tx_info_test import *
-xbridge_rpc_get_tx_info_test.repeat_tx_info_unit_tests(2)
-"""
-
-
-""" TESTME
-from rpc_test_cases.xbridge_rpc_get_createtx_test import *
-xbridge_rpc_get_createtx_test.repeat_tx_info_unit_tests(2)
-"""
-
-
-
-"""            
-        *******************************************************************
-        **********************   CLIENT LAUNCHERS    **********************
-        *******************************************************************
-"""
-
-
-
-"""             INSTRUCTIONS
-
-    - DO NOT RUN FOR NOW.
-
-"""
-
-
-"""                 CLIENT DX_GET_TRANSACTION_INFO
-"""
 
 
 """ RUNS FINE
@@ -167,7 +165,6 @@ xbridge_client_get_tx_info_test.repeat_tx_info_unit_tests(2)
 
 """
 
-
 """ RUNS FINE
 
 xbridge_client_createtx_test.test_createtx_garbage_load_v1(5)
@@ -175,8 +172,8 @@ xbridge_client_createtx_test.test_createtx_garbage_load_v2(5)
 xbridge_client_createtx_test.test_createtx_valid_load(5)
 # Unit tests
 xbridge_client_createtx_test.repeat_create_tx_unit_tests(5)
-"""
 
+"""
 
 """ RUNS FINE
 
@@ -185,14 +182,11 @@ xbridge_client_accept_tx_test.test_accept_garbage_load_v2(5)
 xbridge_client_accept_tx_test.test_accept_valid_load(5)
 # Unit tests
 xbridge_client_accept_tx_test.repeat_accept_tx_unit_tests(5)
-"""
-
-"""                 CLIENT DX_CANCEL_TX
 
 """
-
 
 """ RUNS FINE
+
 xbridge_client_canceltx_test.test_cancel_load_v1(5)
 xbridge_client_canceltx_test.test_cancel_load_v2(5)
 xbridge_client_canceltx_test.test_cancel_load_v3(5)
@@ -200,5 +194,6 @@ xbridge_client_canceltx_test.test_cancel_load_v3(5)
 from test_cases.xbridge_client_canceltx_test import *
 # unittest.main()
 xbridge_client_canceltx_test.repeat_cancel_tx_unit_tests(3)
+
 """
 
