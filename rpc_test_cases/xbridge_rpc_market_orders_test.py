48068c1e2821e6261eaba1438909b6974fa3c25a
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-10-31 18:37:38 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_market_orders_test.py
+++ rpc_test_cases/xbridge_rpc_market_orders_test.py
@@ -4,21 +4,20 @@ from utils import xbridge_utils
 from interface import xbridge_rpc
 
 """
-    - UNORDERED + ORDERED SEQUENCE OF MARKET ACTIONS
-    - CANCELTX + ACCEPT_TX + CREATE_TX
+        - UNORDERED + ORDERED SEQUENCE OF MARKET ACTIONS
+        - CANCELTX + ACCEPT_TX + CREATE_TX
 """
 
 
 """
-    - UNORDERED SEQUENCE OF MARKET ACTIONS
-    - CANCELTX + ACCEPT_TX + CREATE_TX
+        - RANDOM SEQUENCE
 """
 
-def random_seq_market_actions_rpc_calls(nb_of_runs=1000, data_nature=xbridge_utils.RANDOM_VALID_INVALID):
+def random_seq_market_actions_rpc_calls(nb_of_runs=1000, data_nature=xbridge_utils.RANDOM_VALID_INVALID, char_min_size=1, char_max_size=12000):
     time_distribution = []
     total_elapsed_seconds = 0
-    for i in range(1, nb_of_runs):
-        xbridge_utils.generate_new_set_of_data(data_nature)
+    for i in range(1, 1 + nb_of_runs):
+        xbridge_utils.generate_new_set_of_data(data_nature, data_nature, char_min_size, char_max_size)
         func_list = [xbridge_rpc.create_tx, xbridge_rpc.accept_tx, xbridge_rpc.cancel_tx]
         selected_func = random.choice(func_list)
         func_str = ""
@@ -36,19 +35,19 @@ def random_seq_market_actions_rpc_calls(nb_of_runs=1000, data_nature=xbridge_uti
         total_elapsed_seconds += te - ts
         json_str = {"time": te - ts, "API": func_str}
         time_distribution.append(json_str)
-    xbridge_utils.export_data("random_seq_market_actions_rpc_calls.xlsx", time_distribution)
+    xbridge_utils.export_data("random_market_actions_RPC_sequence.xlsx", time_distribution)
 
 
 """
-    - ORDERED SEQUENCE OF MARKET ACTIONS
-    - CANCELTX + ACCEPT_TX + CREATE_TX
+        ***  ORDERED SEQUENCE OF MARKET ACTIONS ***
+        ***  APIs ARE SELECTED SEQUENTIALLY FROM A LIST UNTIL WE REACH THE USER RUN COUNT PARAMETER ***
 """
 
-def defined_seq_market_actions_rpc_calls(nb_of_runs=1000, data_nature=xbridge_utils.RANDOM_VALID_INVALID):
+def defined_seq_market_actions_rpc_calls(nb_of_runs=1000, data_nature=xbridge_utils.RANDOM_VALID_INVALID, char_min_size=1, char_max_size=12000):
     time_distribution = []
     total_elapsed_seconds = 0
-    for i in range(1, nb_of_runs):
-        xbridge_utils.generate_new_set_of_data(data_nature)
+    for i in range(1, 1 + nb_of_runs):
+        xbridge_utils.generate_new_set_of_data(data_nature, data_nature, char_min_size, char_max_size)
         func_str = ""
         ts = time.time()
         if i % 3 == 0:
@@ -64,5 +63,5 @@ def defined_seq_market_actions_rpc_calls(nb_of_runs=1000, data_nature=xbridge_ut
         total_elapsed_seconds += te - ts
         json_str = {"time": te - ts, "API": func_str}
         time_distribution.append(json_str)
-    xbridge_utils.export_data("defined_seq_market_actions_rpc_calls.xlsx", time_distribution)
+    xbridge_utils.export_data("defined_market_actions_RPC_sequence.xlsx", time_distribution)
 
