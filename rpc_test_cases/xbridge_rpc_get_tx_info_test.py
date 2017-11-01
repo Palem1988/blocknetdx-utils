6b8a3b06cd5d0bb9939ea805d00580468a141c8f
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-01 21:31:48 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_get_tx_info_test.py
+++ rpc_test_cases/xbridge_rpc_get_tx_info_test.py
@@ -79,7 +79,7 @@ class get_Tx_Info_UnitTest(unittest.TestCase):
           - We then combine those character classes.
           - Size of the input parameter is fixed.
     """
-    def test_invalid_get_tx_info_test_3(self):
+    def test_invalid_get_tx_info_3(self):
         run_count = 0
         string_length=64
         for itm in [xbridge_utils.one_classes_list, xbridge_utils.two_classes_list, xbridge_utils.three_classes_list, xbridge_utils.four_classes_list, xbridge_utils.five_classes_list]:
@@ -97,7 +97,7 @@ class get_Tx_Info_UnitTest(unittest.TestCase):
     """
           - Same as before, but now the random strings are of random but always very high size [9 000-11 000]
     """
-    def test_invalid_get_tx_info_test_4(self):
+    def test_invalid_get_tx_info_4(self):
         run_count = 0
         string_lower_bound=9000
         string_upper_bound=11000
@@ -117,7 +117,7 @@ class get_Tx_Info_UnitTest(unittest.TestCase):
     """
           - Same as before, but now the random input parameters are of random length [1-4 000]
     """
-    def test_invalid_get_tx_info_test_5(self):
+    def test_invalid_get_tx_info_5(self):
         run_count = 0
         string_lower_bound=1
         string_upper_bound=4000
