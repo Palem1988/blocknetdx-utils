9b19cbf89b707741985cdc917d3447a149c248fc
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-01 09:53:00 +0100

    Add files via upload

--- interface/xbridge_rpc.py
+++ interface/xbridge_rpc.py
@@ -5,18 +5,30 @@ port = '8888'
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:41414"%('Testuser', 'MySuperPassword'))
 
 def get_blockcount():
-    blockcount = rpc_connection.getblockcount()
-    return blockcount
+    try:
+        blockcount = rpc_connection.getblockcount()
+        return blockcount
+    except JSONRPCException:
+        return 0
 
 def get_budget():
-    snode_budget = rpc_connection.mnbudget('show')
-    return snode_budget
+    try:
+        snode_budget = rpc_connection.mnbudget('show')
+        return snode_budget
+    except JSONRPCException:
+        return None
 
 def get_node_list():
-    return rpc_connection.servicenodelist()
+    try:
+        return rpc_connection.servicenodelist()
+    except JSONRPCException:
+        return None
 
 def get_tx(txid):
-    return rpc_connection.getrawtransaction(txid)
+    try:
+        return rpc_connection.getrawtransaction(txid)
+    except JSONRPCException:
+        return None
     
 def send_tx(txid):
     try:
@@ -31,13 +43,22 @@ def sign_tx(txid):
         return None
 
 def decode_raw_tx(txid):
-    return rpc_connection.decoderawtransaction(txid)
+    try:
+        return rpc_connection.decoderawtransaction(txid)
+    except JSONRPCException:
+        return None
 
 def cancel_tx(txid):
-    return rpc_connection.dxCancelTransaction(txid)
+    try:
+        return rpc_connection.dxCancelTransaction(txid)
+    except JSONRPCException:
+        return None
 
 def get_tx_info(txid):
-    return rpc_connection.dxGetTransactionInfo(txid)
+    try:
+        return rpc_connection.dxGetTransactionInfo(txid)
+    except JSONRPCException:
+        return None
 
 def create_tx(fromAddress, fromToken, fromAmount, toAddress, toToken, toAmount):
     try:
@@ -52,13 +73,23 @@ def accept_tx(txid, src, dest):
         return None
 
 def get_currency_list():
-    return rpc_connection.dxGetCurrencyList()
+    try:
+        return rpc_connection.dxGetCurrencyList()
+    except JSONRPCException:
+        return None
 
 def get_transaction_list():
-    return rpc_connection.dxGetTransactionList()
+    try:
+        return rpc_connection.dxGetTransactionList()
+    except JSONRPCException:
+        return None
 
 def get_transaction_history_list():
-    return rpc_connection.dxGetTransactionsHistoryList()
+    try:
+        return rpc_connection.dxGetTransactionsHistoryList()
+    except JSONRPCException:
+        return None
+
 
 """
 rst = get_tx_info("240c472714c1ff14e5f66a6c93ae6f0efb2f4eff593ae31435e829126a0006cc")
