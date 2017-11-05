b213ecee4d02b518ee5f8ecfe28e8b4d1cf3e1cb
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-05 03:02:40 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_wallet_list_test.py
+++ rpc_test_cases/xbridge_rpc_wallet_list_test.py
@@ -13,24 +13,24 @@ from utils import xbridge_utils
 class wallet_List_UnitTest(unittest.TestCase):
     def setUp(self):
         xbridge_utils.generate_new_set_of_data(data_nature=3, char_min_size=1, char_max_size=10000)
-        
+
     def test_listaccounts(self):
-            try:
-                self.assertIsInstance(xbridge_rpc.rpc_connection.listaccounts(), dict)
-            except AssertionError:
-                xbridge_logger.logger.info('listaccounts unit test FAILED')
+        try:
+            self.assertIsInstance(xbridge_rpc.rpc_connection.listaccounts(), dict)
+        except AssertionError:
+            xbridge_logger.logger.info('listaccounts unit test FAILED')
 
     def test_listaddressgroupings(self):
-            try:
-                self.assertIsInstance(xbridge_rpc.rpc_connection.listaddressgroupings(), list)
-            except AssertionError:
-                xbridge_logger.logger.info('listaddressgroupings unit test FAILED')
+        try:
+            self.assertIsInstance(xbridge_rpc.rpc_connection.listaddressgroupings(), list)
+        except AssertionError:
+            xbridge_logger.logger.info('listaddressgroupings unit test FAILED')
 
     def test_listlockunspent(self):
-            try:
-                self.assertIsInstance(xbridge_rpc.rpc_connection.listlockunspent(), list)
-            except AssertionError:
-                xbridge_logger.logger.info('listlockunspent unit test FAILED')
+        try:
+            self.assertIsInstance(xbridge_rpc.rpc_connection.listlockunspent(), list)
+        except AssertionError:
+            xbridge_logger.logger.info('listlockunspent unit test FAILED')
 
     def test_listsinceblock(self):
         try:
