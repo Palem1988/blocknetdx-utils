2f276c42bb27231fbf9e26f22d547074ae27c410
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-05 15:42:05 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_wallet_set_test.py
+++ rpc_test_cases/xbridge_rpc_wallet_set_test.py
@@ -23,6 +23,10 @@ class wallet_Set_UnitTest(unittest.TestCase):
         self.fixed_small_positive_number = 0.00000000000000000000000000000000000000000000000000000001
         self.valid_random_positive_number = xbridge_utils.generate_random_number(0, 1000)
         self.valid_random_positive_int = xbridge_utils.generate_random_int(0, 1000)
+        self.valid_blocknet_address = xbridge_rpc.rpc_connection.getnewaddress()
+        self.invalid_blocknet_address = xbridge_utils.c_src_Address
+        self.invalid_account_str = xbridge_utils.invalid_account_str
+        self.valid_account_str = xbridge_utils.valid_account_str
 
     def test_setfee(self):
         try:
@@ -33,7 +37,7 @@ class wallet_Set_UnitTest(unittest.TestCase):
             self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.settxfee, self.random_neg_number)
             self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.settxfee, self.random_large_positive_nb)
         except AssertionError:
-            xbridge_logger.logger.info('test_setfee unit test FAILED')
+            xbridge_logger.logger.info('****** test_setfee unit test FAILED ******')
 
     # Only int are accepted
     def test_set_stake_split_threshold(self):
@@ -49,8 +53,20 @@ class wallet_Set_UnitTest(unittest.TestCase):
             self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.setstakesplitthreshold, self.positive_float)
             self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.setstakesplitthreshold, xbridge_utils.ca_random_tx_id)
         except AssertionError:
-            xbridge_logger.logger.info('set_stake_split_threshold unit test FAILED')
-            xbridge_logger.logger.info('One of param used: %s' % xbridge_utils.ca_random_tx_id)
+            xbridge_logger.logger.info('****** set_stake_split_threshold unit test FAILED ******\n')
+            xbridge_logger.logger.info('random_tx_id: \n %s \n' % xbridge_utils.ca_random_tx_id)
 
+    def test_set_account(self):
+        try:
+            self.assertIsNone(xbridge_rpc.rpc_connection.setaccount(self.valid_blocknet_address, self.invalid_account_str))
+            self.assertIsNone(xbridge_rpc.rpc_connection.setaccount(self.valid_blocknet_address, self.valid_account_str))
+            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.setaccount, self.invalid_blocknet_address, self.invalid_account_str)
+            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.setaccount, self.invalid_blocknet_address, self.valid_account_str)
+        except AssertionError:
+            xbridge_logger.logger.info('****** setaccount unit test FAILED ****** \n')
+            xbridge_logger.logger.info('invalid_blocknet_address: %s \n' % self.invalid_blocknet_address)
+            xbridge_logger.logger.info('valid_blocknet_address: %s \n' % self.valid_blocknet_address)
+            xbridge_logger.logger.info('invalid_account_str: %s \n' % self.invalid_account_str)
+            xbridge_logger.logger.info('valid_account_str: %s \n' % self.valid_account_str)
 
-# unittest.main()
+unittest.main()
