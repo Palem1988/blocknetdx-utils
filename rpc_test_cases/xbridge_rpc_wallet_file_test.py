cfbc4b8c3f8e209d22589f78eb9d7140e76a48e3
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-06 23:35:07 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_wallet_file_test.py
+++ rpc_test_cases/xbridge_rpc_wallet_file_test.py
@@ -36,23 +36,22 @@ class wallet_File_UnitTest(unittest.TestCase):
     def test_backupwallet(self):
         try:
             log_json = ""
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, " ")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "----")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "{")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "[")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "[]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, "{}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.backupwallet, xbridge_utils.ca_random_tx_id)
-            logstr = {"group": "test_dumpwallet", "success": 1, "error": 0}
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet(""))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet(" "))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet("----"))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet("{"))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet("}"))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet("["))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet("]"))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet("[]"))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet("{}"))
+            self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet(xbridge_utils.ca_random_tx_id))
+            log_json = {"group": "backupwallet", "success": 1, "error": 0}
             xbridge_utils.ERROR_LOG.append(log_json)
         except AssertionError:
-            logstr = {"group": "test_dumpwallet", "success": 0, "error": 1}
+            log_json = {"group": "backupwallet", "success": 0, "error": 1}
             xbridge_utils.ERROR_LOG.append(log_json)
-            xbridge_logger.logger.info('test_dumpwallet unit test FAILED')
+            xbridge_logger.logger.info('backupwallet unit test FAILED')
             xbridge_logger.logger.info('invalid_random_tx_id: %s \n' % xbridge_utils.ca_random_tx_id)
 
-            
-# unittest.main()
+unittest.main()
