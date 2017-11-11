b5e39edda845084d1771a6e19f21f05a057148e9
infinity7592 <kbentahmed@gmail.com>
2017-11-11 21:40:44 +0100

    Update

--- rpc_test_cases/xbridge_rpc_decoderawtx_test.py
+++ rpc_test_cases/xbridge_rpc_decoderawtx_test.py
@@ -4,12 +4,13 @@ BLOCKNET API TESTING TOOLS
 import unittest
 import time
 import xbridge_logger
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 from interface import xbridge_rpc
 from utils import xbridge_utils
+from utils import xbridge_custom_exceptions
 
 from strgen import StringGenerator
-
 
 """
     - Combine optional parameters in a way that generate the test cases you want.
@@ -35,7 +36,6 @@ def decode_raw_tx_RPC_sequence(nb_of_runs=1000, data_nature=3, char_min_size=1, 
 
 
 """                       ***  UNIT TESTS ***
-
 """
 
 class decodeUnitTest(unittest.TestCase):
@@ -46,7 +46,6 @@ class decodeUnitTest(unittest.TestCase):
                 [I] 2017-Oct-31 23:42:48 [0xb8] HTTP: req  decoderawtransaction {"method":"decoderawtransaction","params":["01000000814ef9590108a03740afeb62f97908aba6d79ba512162a0448ead78c1d9acb808b65814914000000006a4730440220111cbcdb9ccdc0e17b65e7fa1e4ec265bf76dcba8202b304b3e34ac833c297b6022008d37ca8ca5ed54bb5d217f5bcdb19c8159059e437a7dbe665c2455b1cfb93fc0121036fa429dbb28304585ebfdae5688bbac546339e578ac4403dcafdc7e15b4f08e1ffffffff0290ab1e000000000017a9145abd1ae96433936b627d249c0c1a57770456323587b0a8db02000000001976a914a5a4651accd909fccaa66e34a7fb5c275c6270ef88ac00000000"],"id":1}
 
                 [I] 2017-Oct-31 23:42:48 [0xb8] HTTP: resp 200 {"result":{"txid":"2e2acb1b085c3b3c5f900153a3280be69a3727b44134342c8a67b0288430f8d6","version":1,"time":1509510785,"locktime":0,"vin":[{"txid":"144981658b80cb9a1d8cd7ea48042a1612a59bd7a6ab0879f962ebaf4037a008","vout":0,"scriptSig":{"asm":"30440220111cbcdb9ccdc0e17b65e7fa1e4ec265bf76dcba8202b304b3e34ac833c297b6022008d37ca8ca5ed54bb5d217f5bcdb19c8159059e437a7dbe665c2455b1cfb93fc01 036fa429dbb28304585ebfdae5688bbac546339e578ac4403dcafdc7e15b4f08e1","hex":"4730440220111cbcdb9ccdc0e17b65e7fa1e4ec265bf76dcba8202b304b3e34ac833c297b6022008d37ca8ca5ed54bb5d217f5bcdb19c8159059e437a7dbe665c2455b1cfb93fc0121036fa429dbb28304585ebfdae5688bbac546339e578ac4403dcafdc7e15b4f08e1"},"sequence":4294967295}],"vout":[{"value":2.01000000,"n":0,"scriptPubKey":{"asm":"OP_HASH160 5abd1ae96433936b627d249c0c1a577704563235 OP_EQUAL","hex":"a9145abd1ae96433936b627d249c0c1a57770456323587","reqSigs":1,"type":"scripthash","addresses":["bM143kSgrZsEksuk2QkiU7zCdGFffyx7iG"]}},{"value":47.95000000,"n":1,"scriptPubKey":{"asm":"OP_DUP OP_HASH160 a5a4651accd909fccaa66e34a7fb5c275c6270ef OP_EQUALVERIFY OP_CHECKSIG","hex":"76a914a5a4651accd909fccaa66e34a7fb5c275c6270ef88ac","reqSigs":1,"type":"pubkeyhash","addresses":["SD4EUkDd5kyiGByjYbGtUPrvHqFjv5nVnZ"]}}]},"error":null,"id":1}
-
             """
             self.assertIsInstance(xbridge_rpc.decode_raw_tx("01000000814ef9590108a03740afeb62f97908aba6d79ba512162a0448ead78c1d9acb808b65814914000000006a4730440220111cbcdb9ccdc0e17b65e7fa1e4ec265bf76dcba8202b304b3e34ac833c297b6022008d37ca8ca5ed54bb5d217f5bcdb19c8159059e437a7dbe665c2455b1cfb93fc0121036fa429dbb28304585ebfdae5688bbac546339e578ac4403dcafdc7e15b4f08e1ffffffff0290ab1e000000000017a9145abd1ae96433936b627d249c0c1a57770456323587b0a8db02000000001976a914a5a4651accd909fccaa66e34a7fb5c275c6270ef88ac00000000"), dict)
             # print("decode_raw_tx Valid Unit Test Group 1 OK")
@@ -58,24 +57,24 @@ class decodeUnitTest(unittest.TestCase):
             - Basic tests
     """
     def test_invalid_decode_1(self):
-        try:
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx(" "), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx(""), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("[]"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("[[]]"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("{{}}"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("{[]}"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("[{[]}]"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("["), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("{"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("]"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("}"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("''"), dict)
-            self.assertIsInstance(xbridge_rpc.decode_raw_tx("'"), dict)
-            # print("decode_raw_tx Unit Test Group 1 OK")
-        except AssertionError as e:
-            # print("****** decode_raw_tx Unit Test Group 1 FAILED ******")
-            xbridge_logger.logger.info('decode_raw_tx unit test group 1 FAILED')
+        for basic_garbage_str in xbridge_utils.set_of_invalid_parameters:
+            with self.subTest(basic_garbage_str=basic_garbage_str):
+                try:
+                    if basic_garbage_str in (9999999999999999999999999999999999999999999999999999999999999999, -9999999999999999999999999999999999999999999999999999999999999999):
+                        self.assertIsNone(xbridge_rpc.decode_raw_tx(basic_garbage_str))
+                    else:
+                        self.assertRaises(xbridge_custom_exceptions.ValidBlockNetException, xbridge_rpc.decode_raw_tx, basic_garbage_str)
+                    log_json = {"group": "test_invalid_decode_1", "success": 1, "failure": 0, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                except AssertionError as ass_err:
+                    xbridge_logger.logger.info('test_invalid_decode_1 unit test FAILED: %s' % ass_err)
+                    log_json = {"group": "test_invalid_decode_1", "success": 0, "failure": 1, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('basic_garbage_str: %s \n' % basic_garbage_str)
+                except JSONRPCException as json_excpt:
+                    log_json = {"group": "test_invalid_decode_1", "success": 0,  "failure": 0, "error": 1}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_invalid_decode_1 unit test ERROR: %s' % str(json_excpt))
 
     """
           - We test various random inputs from individual character classes.
@@ -83,7 +82,6 @@ class decodeUnitTest(unittest.TestCase):
           - Size of the input parameter is fixed.
     """
     def test_invalid_decode_2(self):
-        run_count = 0
         string_length=64
         for itm in [xbridge_utils.one_classes_list, xbridge_utils.two_classes_list, xbridge_utils.three_classes_list, xbridge_utils.four_classes_list, xbridge_utils.five_classes_list]:
             for sub_item in itm:
@@ -92,21 +90,25 @@ class decodeUnitTest(unittest.TestCase):
                     try:
                         generated_str = StringGenerator(clss_str).render()
                         self.assertIsInstance(xbridge_rpc.decode_raw_tx(generated_str), dict)
-                        run_count += 1
+                        log_json = {"group": "test_invalid_decode_2", "success": 1, "failure": 0, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
                     except AssertionError as e:
-                        # print("****** decode_raw_tx Unit SUBTEST 2 FAILED ON PARAMETER: %s ******" % generated_str)
                         xbridge_logger.logger.info('decode_raw_tx unit test group 2 FAILED on parameter: %s', generated_str)
-                        run_count += 1
-        # print("UT Group 2 - total subtests completed with or without errors: %s" % str(run_count))
+                        log_json = {"group": "test_invalid_decode_2", "success": 0, "failure": 1, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('basic_garbage_str: %s \n' % generated_str)
+                    except JSONRPCException as json_excpt:
+                        log_json = {"group": "test_invalid_decode_2", "success": 0, "failure": 0, "error": 1}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('test_invalid_decode_2 unit test ERROR: %s' % str(json_excpt))
 
-                    
+
     """
           - Same as before, but now the random strings are of random but always very long size [9 000-11 000]
     """
     def test_invalid_decode_3(self):
         string_lower_bound=9000
         string_upper_bound=11000
-        run_count = 0
         for itm in [xbridge_utils.one_classes_list, xbridge_utils.two_classes_list, xbridge_utils.three_classes_list, xbridge_utils.four_classes_list, xbridge_utils.five_classes_list]:
             for sub_item in itm:
                 with self.subTest(sub_item=sub_item):
@@ -114,12 +116,17 @@ class decodeUnitTest(unittest.TestCase):
                     try:
                         generated_str = StringGenerator(clss_str).render()
                         self.assertIsInstance(xbridge_rpc.decode_raw_tx(generated_str), dict)
-                        run_count += 1
+                        log_json = {"group": "test_invalid_decode_3", "success": 1, "failure": 0, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
                     except AssertionError as e:
-                        # print("****** decode_raw_tx Unit SUBTEST 3 FAILED ON PARAMETER: %s ******" % generated_str)
                         xbridge_logger.logger.info('decode_raw_tx unit test group 3 FAILED on parameter: %s', generated_str)
-                        run_count += 1
-        # print("UT Group 3 - total subtests completed with or without errors: %s" % str(run_count))
+                        log_json = {"group": "test_invalid_decode_3", "success": 0, "failure": 1, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('basic_garbage_str: %s \n' % generated_str)
+                    except JSONRPCException as json_excpt:
+                        log_json = {"group": "test_invalid_decode_3", "success": 0, "failure": 0, "error": 1}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('test_invalid_decode_3 unit test ERROR: %s' % str(json_excpt))
 
                             
     """
@@ -136,12 +143,17 @@ class decodeUnitTest(unittest.TestCase):
                     try:
                         generated_str = StringGenerator(clss_str).render()
                         self.assertIsInstance(xbridge_rpc.decode_raw_tx(generated_str), dict)
-                        run_count += 1
+                        log_json = {"group": "test_invalid_decode_4", "success": 1, "failure": 0, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
                     except AssertionError as e:
-                        # print("****** decode_raw_tx Unit SUBTEST 4 FAILED ON PARAMETER: %s ******" % generated_str)
                         xbridge_logger.logger.info('decode_raw_tx unit test group 4 FAILED on parameter: %s', generated_str)
-                        run_count += 1
-        # print("UT Group 4 - total subtests completed with or without errors: %s" % str(run_count))
+                        log_json = {"group": "test_invalid_decode_4", "success": 0, "failure": 1, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('basic_garbage_str: %s \n' % generated_str)
+                    except JSONRPCException as json_excpt:
+                        log_json = {"group": "test_invalid_decode_4", "success": 0, "failure": 0, "error": 1}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('test_invalid_decode_4 unit test ERROR: %s' % str(json_excpt))
 
 
 """
@@ -155,4 +167,6 @@ def repeat_decode_raw_tx_unit_tests(runs=1000):
 """
 if __name__ == '__main__':
     unittest.main()
+
+unittest.main()
 """
--- rpc_test_cases/xbridge_rpc_sendtx_test.py
+++ rpc_test_cases/xbridge_rpc_sendtx_test.py
@@ -9,6 +9,8 @@ from interface import xbridge_rpc
 from utils import xbridge_utils
 
 from strgen import StringGenerator
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from utils import xbridge_custom_exceptions
 
 
 """
@@ -51,24 +53,24 @@ class sendUnitTest(unittest.TestCase):
             - Basic tests
     """
     def test_invalid_send_1(self):
-        try:
-            self.assertIsNone(xbridge_rpc.send_tx(" "))
-            self.assertIsNone(xbridge_rpc.send_tx(""))
-            self.assertIsNone(xbridge_rpc.send_tx("[]"))
-            self.assertIsNone(xbridge_rpc.send_tx("[[]]"))
-            self.assertIsNone(xbridge_rpc.send_tx("{{}}"))
-            self.assertIsNone(xbridge_rpc.send_tx("{[]}"))
-            self.assertIsNone(xbridge_rpc.send_tx("[{[]}]"))
-            self.assertIsNone(xbridge_rpc.send_tx("["))
-            self.assertIsNone(xbridge_rpc.send_tx("{"))
-            self.assertIsNone(xbridge_rpc.send_tx("]"))
-            self.assertIsNone(xbridge_rpc.send_tx("}"))
-            self.assertIsNone(xbridge_rpc.send_tx("''"))
-            self.assertIsNone(xbridge_rpc.send_tx("'"))
-            # print("dxSend Unit Test Group 1 OK")
-        except AssertionError as e:
-            # print("****** dxSend Unit Test Group 1 FAILED ******")
-            xbridge_logger.logger.info('dxSend unit test group 1 FAILED')
+        for basic_garbage_str in xbridge_utils.set_of_invalid_parameters:
+            with self.subTest(basic_garbage_str=basic_garbage_str):
+                try:
+                    if basic_garbage_str in (9999999999999999999999999999999999999999999999999999999999999999, -9999999999999999999999999999999999999999999999999999999999999999):
+                        self.assertIsNone(xbridge_rpc.decode_raw_tx(basic_garbage_str))
+                    else:
+                        self.assertRaises(xbridge_custom_exceptions.ValidBlockNetException, xbridge_rpc.send_tx, basic_garbage_str)
+                    log_json = {"group": "test_invalid_send_1", "success": 1, "failure": 0, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                except AssertionError as ass_err:
+                    xbridge_logger.logger.info('test_invalid_send_1 unit test FAILED: %s' % ass_err)
+                    log_json = {"group": "test_invalid_send_1", "success": 0, "failure": 1, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('basic_garbage_str: %s \n' % basic_garbage_str)
+                except JSONRPCException as json_excpt:
+                    log_json = {"group": "test_invalid_send_1", "success": 0,  "failure": 0, "error": 1}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_invalid_send_1 unit test ERROR: %s' % str(json_excpt))
 
     """
           - We test various random inputs from individual character classes.
@@ -76,7 +78,6 @@ class sendUnitTest(unittest.TestCase):
           - Size of the input parameter is fixed.
     """
     def test_invalid_send_2(self):
-        run_count = 0
         string_length=64
         for itm in [xbridge_utils.one_classes_list, xbridge_utils.two_classes_list, xbridge_utils.three_classes_list, xbridge_utils.four_classes_list, xbridge_utils.five_classes_list]:
             for sub_item in itm:
@@ -85,12 +86,17 @@ class sendUnitTest(unittest.TestCase):
                     try:
                         generated_str = StringGenerator(clss_str).render()
                         self.assertIsNone(xbridge_rpc.send_tx(generated_str))
-                        run_count += 1
-                    except AssertionError as e:
-                        # print("****** dxSend Unit SUBTEST 2 FAILED ON PARAMETER: %s ******" % generated_str)
-                        xbridge_logger.logger.info('dxSend unit test group 2 FAILED on parameter: %s', generated_str)
-                        run_count += 1
-        # print("UT Group 2 - total subtests completed with or without errors: %s" % str(run_count))
+                        log_json = {"group": "test_invalid_send_2", "success": 1, "failure": 0, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                    except AssertionError as ass_err:
+                        log_json = {"group": "test_invalid_send_2", "success": 0, "failure": 1, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('test_invalid_send_2 FAILED on parameter: %s', ass_err)
+                        xbridge_logger.logger.info('param: %s', generated_str)
+                    except JSONRPCException as json_excpt:
+                        log_json = {"group": "test_invalid_send_2", "success": 0, "failure": 0, "error": 1}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('test_invalid_send_2 unit test ERROR: %s' % str(json_excpt))
 
                     
     """
@@ -99,7 +105,6 @@ class sendUnitTest(unittest.TestCase):
     def test_invalid_send_3(self):
         string_lower_bound=9000
         string_upper_bound=11000
-        run_count = 0
         for itm in [xbridge_utils.one_classes_list, xbridge_utils.two_classes_list, xbridge_utils.three_classes_list, xbridge_utils.four_classes_list, xbridge_utils.five_classes_list]:
             for sub_item in itm:
                 with self.subTest(sub_item=sub_item):
@@ -107,19 +112,23 @@ class sendUnitTest(unittest.TestCase):
                     try:
                         generated_str = StringGenerator(clss_str).render()
                         self.assertIsNone(xbridge_rpc.send_tx(generated_str))
-                        run_count += 1
-                    except AssertionError as e:
-                        # print("****** dxSend Unit SUBTEST 3 FAILED ON PARAMETER: %s ******" % generated_str)
-                        xbridge_logger.logger.info('dxSend unit test group 3 FAILED on parameter: %s', generated_str)
-                        run_count += 1
-        # print("UT Group 3 - total subtests completed with or without errors: %s" % str(run_count))
+                        log_json = {"group": "test_invalid_send_3", "success": 1, "failure": 0, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                    except AssertionError as ass_err:
+                        xbridge_logger.logger.info('test_invalid_send_3 FAILED on parameter: %s', ass_err)
+                        xbridge_logger.logger.info('param: %s', generated_str)
+                        log_json = {"group": "test_invalid_send_3", "success": 0, "failure": 1, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                    except JSONRPCException as json_excpt:
+                        log_json = {"group": "test_invalid_send_3", "success": 0, "failure": 0, "error": 1}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('test_invalid_send_1 unit test ERROR: %s' % str(json_excpt))
 
                             
     """
           - Same as before, but now the random input parameters are of random length [1-4 000]
     """
     def test_invalid_send_4(self):
-        run_count = 0
         string_lower_bound=1
         string_upper_bound=4000
         for itm in [xbridge_utils.one_classes_list, xbridge_utils.two_classes_list, xbridge_utils.three_classes_list, xbridge_utils.four_classes_list, xbridge_utils.five_classes_list]:
@@ -129,12 +138,17 @@ class sendUnitTest(unittest.TestCase):
                     try:
                         generated_str = StringGenerator(clss_str).render()
                         self.assertIsNone(xbridge_rpc.send_tx(generated_str))
-                        run_count += 1
-                    except AssertionError as e:
-                        # print("****** dxSend Unit SUBTEST 4 FAILED ON PARAMETER: %s ******" % generated_str)
-                        xbridge_logger.logger.info('dxSend unit test group 4 FAILED on parameter: %s', generated_str)
-                        run_count += 1
-        # print("UT Group 4 - total subtests completed with or without errors: %s" % str(run_count))
+                        log_json = {"group": "test_invalid_send_4", "success": 1, "failure": 0, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                    except AssertionError as ass_err:
+                        xbridge_logger.logger.info('test_invalid_send_4 FAILED on parameter: %s', ass_err)
+                        xbridge_logger.logger.info('param: %s', generated_str)
+                        log_json = {"group": "test_invalid_send_4", "success": 0, "failure": 1, "error": 0}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                    except JSONRPCException as json_excpt:
+                        log_json = {"group": "test_invalid_send_4", "success": 0, "failure": 0, "error": 1}
+                        xbridge_utils.ERROR_LOG.append(log_json)
+                        xbridge_logger.logger.info('test_invalid_send_1 unit test ERROR: %s' % str(json_excpt))
 
 
 """
@@ -148,6 +162,7 @@ def repeat_send_tx_unit_tests(runs=1000):
 """
 if __name__ == '__main__':
     unittest.main()
+
+unittest.main()
 """
 
-# unittest.main()
