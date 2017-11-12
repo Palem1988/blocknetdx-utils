6faa631c6f97e662aba422d3cb142b64ca222490
infinity7592 <kbentahmed@gmail.com>
2017-11-12 16:12:03 +0100

    Update

--- utils/xbridge_ref.py
+++ utils/xbridge_ref.py
@@ -1,10 +1,8 @@
+
 from rpc_test_cases import xbridge_rpc_accept_tx_test
 from rpc_test_cases import xbridge_rpc_canceltx_test
 from rpc_test_cases import xbridge_rpc_get_tx_info_test
 from rpc_test_cases import xbridge_rpc_createtx_test
-from rpc_test_cases import xbridge_rpc_market_orders_test
-from rpc_test_cases import xbridge_rpc_heavy_polling_test
-from rpc_test_cases import xbridge_rpc_sequence_test
 from rpc_test_cases import xbridge_rpc_misc_test
 
 from rpc_test_cases import xbridge_rpc_signtx_test
@@ -13,11 +11,14 @@ from rpc_test_cases import xbridge_rpc_sendtx_test
 from rpc_test_cases import xbridge_rpc_blockchain_test
 from rpc_test_cases import xbridge_rpc_blocknetdx_test
 from rpc_test_cases import xbridge_rpc_network_test
-from rpc_test_cases import xbridge_rpc_wallet_list_test
-from rpc_test_cases import xbridge_rpc_wallet_set_test
 from rpc_test_cases import xbridge_rpc_mining_test
 
+from rpc_test_cases import xbridge_rpc_wallet_list_test
+from rpc_test_cases import xbridge_rpc_wallet_set_test
+from rpc_test_cases import xbridge_rpc_wallet_get_test
+
 from rpc_test_cases import xbridge_rpc_send_test
+
 
 unit_tests_module_strings = [xbridge_rpc_createtx_test,
                             xbridge_rpc_canceltx_test,
@@ -30,7 +31,43 @@ unit_tests_module_strings = [xbridge_rpc_createtx_test,
                             xbridge_rpc_blocknetdx_test,
                             xbridge_rpc_network_test,
                             xbridge_rpc_wallet_list_test,
+                            xbridge_rpc_wallet_get_test,
                             xbridge_rpc_wallet_set_test,
                             xbridge_rpc_mining_test,
                             xbridge_rpc_send_test
                             ]
+
+unit_tests_class_names = [xbridge_rpc_misc_test.Misc_UnitTest,
+                          xbridge_rpc_accept_tx_test.accept_Tx_Test,
+                          xbridge_rpc_send_test.send_UnitTest,
+                          xbridge_rpc_createtx_test.create_Tx_Test,
+                          xbridge_rpc_canceltx_test.cancelUnitTest,
+                          xbridge_rpc_get_tx_info_test.get_Tx_Info_UnitTest,
+                          xbridge_rpc_signtx_test.signUnitTest,
+                          xbridge_rpc_sendtx_test.sendUnitTest,
+                          xbridge_rpc_blockchain_test.Blockchain_UnitTest,
+                          xbridge_rpc_blocknetdx_test.Blocknetdx_UnitTest,
+                          xbridge_rpc_network_test.Network_UnitTest,
+                          xbridge_rpc_wallet_list_test.wallet_List_UnitTest,
+                          xbridge_rpc_wallet_get_test.wallet_get_UnitTest,
+                          xbridge_rpc_wallet_set_test.wallet_Set_UnitTest,
+                          xbridge_rpc_mining_test.Mining_UnitTest
+                          ]
+
+polling_UT_class_names = [xbridge_rpc_get_tx_info_test.get_Tx_Info_UnitTest,
+                          xbridge_rpc_wallet_list_test.wallet_List_UnitTest,
+                          xbridge_rpc_wallet_get_test.wallet_get_UnitTest
+                          ]
+
+market_actions_UT_class_names = [
+    xbridge_rpc_accept_tx_test.accept_Tx_Test,
+    xbridge_rpc_createtx_test.create_Tx_Test,
+    xbridge_rpc_canceltx_test.cancelUnitTest
+]
+
+market_and_polling_UT_class_names = [
+    xbridge_rpc_accept_tx_test.accept_Tx_Test,
+    xbridge_rpc_createtx_test.create_Tx_Test,
+    xbridge_rpc_canceltx_test.cancelUnitTest,
+    xbridge_rpc_get_tx_info_test.get_Tx_Info_UnitTest,
+]
