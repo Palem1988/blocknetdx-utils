8698c1b55a43d3f0b7fec2b1c47428973af5428c
infinity7592 <kbentahmed@gmail.com>
2017-11-12 22:02:42 +0100

    Update

--- rpc_test_cases/xbridge_rpc_mining_test.py
+++ rpc_test_cases/xbridge_rpc_mining_test.py
@@ -1,7 +1,6 @@
 import unittest
 import xbridge_logger
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-from decimal import *
 
 from interface import xbridge_rpc
 from utils import xbridge_utils
@@ -11,22 +10,23 @@ from utils import xbridge_utils
 
 class Mining_UnitTest(unittest.TestCase):
     def setUp(self):
-        xbridge_utils.generate_new_set_of_data(data_nature=3, char_min_size=1, char_max_size=10000)
+        xbridge_utils.generate_new_set_of_data(data_nature=xbridge_utils.INVALID_DATA, char_min_size=1, char_max_size=10000)
 
     def test_submitblock(self):
-        try:
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, " ")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "----")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "{")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "[")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "[]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, "{}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock,
-                              xbridge_utils.ca_random_tx_id)
-        except AssertionError:
-            xbridge_logger.logger.info('submitblock unit test FAILED')
+        for basic_garbage_str in xbridge_utils.set_of_invalid_parameters:
+            with self.subTest(basic_garbage_str=basic_garbage_str):
+                try:
+                    self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.submitblock, basic_garbage_str)
+                    log_json = {"group": "test_submitblock", "success": 1, "failure": 0, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                except AssertionError as ass_err:
+                    log_json = {"group": "test_submitblock", "success": 0, "failure": 1, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_submitblock unit test FAILED: %s' % ass_err)
+                    xbridge_logger.logger.info('basic_garbage_str: %s \n' % basic_garbage_str)
+                except JSONRPCException as json_excpt:
+                    log_json = {"group": "test_submitblock", "success": 0,  "failure": 0, "error": 1}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_submitblock unit test ERROR: %s' % str(json_excpt))
 
 # unittest.main()
