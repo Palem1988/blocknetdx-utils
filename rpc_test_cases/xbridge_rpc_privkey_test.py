8c1718e0a946323cb723ef9c57e3ba71d01c005b
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-13 20:24:44 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_privkey_test.py
+++ rpc_test_cases/xbridge_rpc_privkey_test.py
@@ -1,12 +1,14 @@
 import unittest
 import xbridge_logger
+import random
+
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-from decimal import *
+from utils import xbridge_custom_exceptions
 
 from interface import xbridge_rpc
 from utils import xbridge_utils
 
-"""                       ***  UNIT TESTS FOR ALL WALLET RELATED FUNCTIONS STARTING WITH GET ***
+"""                       ***  UNIT TESTS ***
 """
 
 class private_Key_UnitTest(unittest.TestCase):
@@ -15,53 +17,99 @@ class private_Key_UnitTest(unittest.TestCase):
 
     # importprivkey "blocknetdxprivkey" ( "label" rescan )
     # @unittest.skip("disabled - not tested")
-    def test_importprivkey(self):
+    def test_importprivkey_invalid(self):
+        modified_set = [x for x in xbridge_utils.set_of_invalid_parameters if x not in (-9999999999999999999999999999999999999999999999999999999999999999, 9999999999999999999999999999999999999999999999999999999999999999)]
+        for basic_garbage_str in modified_set:
+            with self.subTest(basic_garbage_str=basic_garbage_str):
+                try:
+                    blocknetdxprivkey = random.choice(modified_set)
+                    optional_label = random.choice(modified_set)
+                    optional_rescan = random.choice(modified_set)
+                    if random.choice(["", modified_set]) == "":
+                        optional_label = None
+                    else:
+                        optional_label = random.choice(modified_set)
+                    if random.choice(["", modified_set]) == "":
+                        optional_rescan = None
+                    else:
+                        optional_rescan = random.choice(modified_set)
+                    self.assertRaises(xbridge_custom_exceptions.ValidBlockNetException, xbridge_rpc.importprivkey, blocknetdxprivkey, optional_label, optional_rescan)
+                    log_json = {"group": "test_importprivkey_invalid", "success": 1, "failure": 0, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                except AssertionError as ass_err:
+                    log_json = {"group": "test_importprivkey_invalid", "success": 0, "failure": 1, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_importprivkey_invalid FAILED: %s' % ass_err)
+                    xbridge_logger.logger.info('blocknetdxprivkey: %s \n' % blocknetdxprivkey)
+                    xbridge_logger.logger.info('optional_label: %s \n' % optional_label)
+                    xbridge_logger.logger.info('optional_rescan: %s \n' % optional_rescan)
+                except JSONRPCException as json_excpt:
+                    log_json = {"group": "test_importprivkey_invalid", "success": 0,  "failure": 0, "error": 1}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_importprivkey_invalid ERROR: %s' % str(json_excpt))
+
+    # importprivkey "blocknetdxprivkey" ( "label" rescan )
+    @unittest.skip("DISABLED - IN PROGRESS - UNTESTED")
+    def test_importprivkey_valid(self):
         try:
             log_json = ""
-            valid_blocknetdx_address = xbridge_rpc.rpc_connection.getnewaddress()
-            self.assertIsInstance(xbridge_rpc.rpc_connection.dumpprivkey(valid_blocknetdx_address), dict)
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, " ")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "----")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "{")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "[")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "[]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, "{}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.importprivkey, xbridge_utils.ca_random_tx_id)
-            log_json = {"group": "test_importprivkey", "success": 1, "error": 0}
+            valid_blocknetdx_address = xbridge_utils.generate_valid_blocknet_address()
+            self.assertIsInstance(xbridge_rpc.rpc_connection.importprivkey(valid_blocknetdx_address), dict)
+            log_json = {"group": "test_importprivkey_valid", "success": 1, "failure": 0, "error": 0}
             xbridge_utils.ERROR_LOG.append(log_json)
-        except AssertionError:
-            log_json = {"group": "test_importprivkey", "success": 0, "error": 1}
+        except AssertionError as ass_err:
+            log_json = {"group": "test_importprivkey_valid", "success": 0, "failure": 1, "error": 0}
             xbridge_utils.ERROR_LOG.append(log_json)
-            xbridge_logger.logger.info('test_importprivkey unit test FAILED')
+            xbridge_logger.logger.info('test_importprivkey_valid FAILED: %s' % ass_err)
             xbridge_logger.logger.info('valid_blocknetdx_address: %s \n' % valid_blocknetdx_address)
-            xbridge_logger.logger.info('invalid_random_tx_id: %s \n' % xbridge_utils.ca_random_tx_id)
+        except JSONRPCException as json_excpt:
+            log_json = {"group": "test_importprivkey_valid", "success": 0,  "failure": 0, "error": 1}
+            xbridge_utils.ERROR_LOG.append(log_json)
+            xbridge_logger.logger.info('test_importprivkey_valid ERROR: %s' % str(json_excpt))
+            xbridge_logger.logger.info('valid_blocknetdx_address: %s \n' % valid_blocknetdx_address)
+    
+     # dumpprivkey "blocknetdxaddress"
+    def test_dumpprivkey_invalid(self):
+        self.assertIsNone(xbridge_rpc.dumpprivkey(-9999999999999999999999999999999999999999999999999999999999999999))
+        self.assertIsNone(xbridge_rpc.dumpprivkey(9999999999999999999999999999999999999999999999999999999999999999))
+        modified_set = [x for x in xbridge_utils.set_of_invalid_parameters if x not in (-9999999999999999999999999999999999999999999999999999999999999999, 9999999999999999999999999999999999999999999999999999999999999999)]
+        for basic_garbage_str in modified_set:
+            with self.subTest(basic_garbage_str=basic_garbage_str):
+                try:
+                    if isinstance(basic_garbage_str, str):
+                        self.assertIsNone(xbridge_rpc.dumpprivkey(basic_garbage_str))
+                    else:
+                        self.assertRaises(xbridge_custom_exceptions.ValidBlockNetException, xbridge_rpc.dumpprivkey, basic_garbage_str)
+                    log_json = {"group": "test_dumpprivkey_invalid", "success": 1, "failure": 0, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                except AssertionError as ass_err:
+                    log_json = {"group": "test_dumpprivkey_invalid", "success": 0, "failure": 1, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_dumpprivkey_invalid FAILED: %s' % ass_err)
+                    xbridge_logger.logger.info('basic_garbage_str: %s \n' % basic_garbage_str)
+                except JSONRPCException as json_excpt:
+                    log_json = {"group": "test_dumpprivkey_invalid", "success": 0,  "failure": 0, "error": 1}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_dumpprivkey_invalid ERROR: %s' % str(json_excpt))
 
     # dumpprivkey "blocknetdxaddress"
-    def test_dumpprivkey(self):
+    # FAILED: 'PnXVPpt8TR8P3ZYn6aUDPcvjPhcigsASAxy2c8NEjaKJky4McBG4' is not an instance of <class 'dict'>
+    # basic_garbage_str: BcS8oz3HYNuJqSzYLkbGkjGzxVq6nhaJrC
+    def test_dumpprivkey_valid(self):
         try:
             log_json = ""
-            valid_blocknetdx_address = xbridge_rpc.rpc_connection.getnewaddress()
-            self.assertIsInstance(xbridge_rpc.rpc_connection.dumpprivkey(valid_blocknetdx_address), dict)
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, " ")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "----")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "{")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "[")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "[]")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, "{}")
-            self.assertRaises(JSONRPCException, xbridge_rpc.rpc_connection.dumpprivkey, xbridge_utils.ca_random_tx_id)
-            log_json = {"group": "test_dumpprivkey", "success": 1, "error": 0}
+            valid_blocknetdx_address = xbridge_utils.generate_valid_blocknet_address()
+            self.assertIsInstance(xbridge_rpc.rpc_connection.dumpprivkey(valid_blocknetdx_address), str)
+            log_json = {"group": "test_dumpprivkey_valid", "success": 1, "failure": 0, "error": 0}
             xbridge_utils.ERROR_LOG.append(log_json)
-        except AssertionError:
-            log_json = {"group": "test_dumpprivkey", "success": 0, "error": 1}
+        except AssertionError as ass_err:
+            log_json = {"group": "test_dumpprivkey_valid", "success": 0, "failure": 1, "error": 0}
             xbridge_utils.ERROR_LOG.append(log_json)
-            xbridge_logger.logger.info('test_dumpprivkey unit test FAILED')
-            xbridge_logger.logger.info('valid_blocknetdx_address: %s \n' % valid_blocknetdx_address)
-            xbridge_logger.logger.info('invalid_random_tx_id: %s \n' % xbridge_utils.ca_random_tx_id)
-
+            xbridge_logger.logger.info('test_dumpprivkey_valid FAILED: %s' % ass_err)
+            xbridge_logger.logger.info('basic_garbage_str: %s \n' % valid_blocknetdx_address)
+        except JSONRPCException as json_excpt:
+            log_json = {"group": "test_dumpprivkey_valid", "success": 0,  "failure": 0, "error": 1}
+            xbridge_utils.ERROR_LOG.append(log_json)
+            xbridge_logger.logger.info('test_dumpprivkey_valid ERROR: %s' % str(json_excpt))
+    
 # unittest.main()
