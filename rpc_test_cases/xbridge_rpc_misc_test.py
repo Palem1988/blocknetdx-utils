78e3a690e75ffefe801433d6cbd32c2b228cff41
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-16 20:56:35 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_misc_test.py
+++ rpc_test_cases/xbridge_rpc_misc_test.py
@@ -7,16 +7,60 @@ from utils import xbridge_custom_exceptions
 from interface import xbridge_rpc
 from utils import xbridge_utils
 
-subTest_count = 10
+import sys
+sys.path.insert(0,'..')
+import xbridge_config
+
+subTest_count = xbridge_config.get_conf_subtests_run_number()
 
 class Misc_UnitTest(unittest.TestCase):
     def setUp(self):
         xbridge_utils.generate_new_set_of_data(data_nature=xbridge_utils.INVALID_DATA, char_min_size=1, char_max_size=10000)
 
+    def test_bip38encrypt_invalid(self):
+        for i in range(subTest_count):
+            log_json = ""
+            with self.subTest("test_bip38encrypt_invalid"):
+                try:
+                    invalid_blocknet_address = random.choice(xbridge_utils.set_of_invalid_parameters)
+                    self.assertRaises(xbridge_custom_exceptions.ValidBlockNetException, xbridge_rpc.bip38encrypt, invalid_blocknet_address)
+                    log_json = {"group": "test_bip38encrypt_invalid", "success": 1, "failure": 0, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                except AssertionError as ass_err:
+                    log_json = {"group": "test_bip38encrypt_invalid", "success": 0, "failure": 1, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_bip38encrypt_invalid FAILED: %s' % ass_err)
+                    xbridge_logger.logger.info('invalid_blocknet_address: %s \n' % invalid_blocknet_address)
+                except JSONRPCException as json_excpt:
+                    log_json = {"group": "test_bip38encrypt_invalid", "success": 0, "failure": 0, "error": 1}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_bip38encrypt_invalid ERROR: %s' % json_excpt)
+                    xbridge_logger.logger.info('invalid_blocknet_address: %s \n' % invalid_blocknet_address)
+    
+    def test_bip38decrypt_invalid(self):
+        for i in range(subTest_count):
+            log_json = ""
+            with self.subTest("test_bip38decrypt_invalid"):
+                try:
+                    invalid_blocknet_address = random.choice(xbridge_utils.set_of_invalid_parameters)
+                    self.assertRaises(xbridge_custom_exceptions.ValidBlockNetException, xbridge_rpc.bip38decrypt,
+                                      invalid_blocknet_address)
+                    log_json = {"group": "test_bip38decrypt_invalid", "success": 1, "failure": 0, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                except AssertionError as ass_err:
+                    log_json = {"group": "test_bip38decrypt_invalid", "success": 0, "failure": 1, "error": 0}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_bip38decrypt_invalid FAILED: %s' % ass_err)
+                    xbridge_logger.logger.info('invalid_blocknet_address: %s \n' % invalid_blocknet_address)
+                except JSONRPCException as json_excpt:
+                    log_json = {"group": "test_bip38decrypt_invalid", "success": 0, "failure": 0, "error": 1}
+                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.logger.info('test_bip38decrypt_invalid ERROR: %s' % json_excpt)
+                    xbridge_logger.logger.info('invalid_blocknet_address: %s \n' % invalid_blocknet_address)
+    
     def test_getinfo(self):
         try:
             log_json = ""
-            xbridge_utils.generate_new_set_of_data(data_nature=xbridge_utils.VALID_DATA)            
             self.assertIsInstance(xbridge_rpc.rpc_connection.getinfo(), dict)
             log_json = {"group": "test_getinfo", "success": 1, "failure": 0, "error": 0}
             xbridge_utils.ERROR_LOG.append(log_json)
@@ -26,8 +70,7 @@ class Misc_UnitTest(unittest.TestCase):
         except JSONRPCException as json_excpt:
             log_json = {"group": "test_getinfo", "success": 0, "failure": 0, "error": 1}
             xbridge_utils.ERROR_LOG.append(log_json)
-        
-        
+
     # signmessage "blocknetdxaddress" "message"
     # Please enter the wallet passphrase with walletpassphrase first.
     # TODO: valid test
@@ -143,8 +186,11 @@ class Misc_UnitTest(unittest.TestCase):
 
 """
 suite = unittest.TestSuite()
-# suite.addTest(Misc_UnitTest("test_autocombinerewards_valid"))
-suite.addTest(Misc_UnitTest("test_signmessage_invalid"))
+for i in range(20):
+    suite.addTest(Misc_UnitTest("test_bip38encrypt_invalid"))
+    suite.addTest(Misc_UnitTest("test_bip38decrypt_invalid"))
+    # suite.addTest(Misc_UnitTest("test_signmessage_invalid"))
 runner = unittest.TextTestRunner()
 runner.run(suite)
 """
+
