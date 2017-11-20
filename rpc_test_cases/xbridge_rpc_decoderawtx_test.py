228ecdeb794f293df4309ba6b094b29ac4f16b1e
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-20 19:32:18 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_decoderawtx_test.py
+++ rpc_test_cases/xbridge_rpc_decoderawtx_test.py
@@ -1,6 +1,3 @@
-"""
-BLOCKNET API TESTING TOOLS
-"""
 import unittest
 import time
 import xbridge_logger
@@ -13,7 +10,11 @@ from utils import xbridge_custom_exceptions
 from strgen import StringGenerator
 
 class decodeUnitTest(unittest.TestCase):
-    def test_valid_tx_id_1(self):
+    def setUp(self):
+       xbridge_utils.generate_new_set_of_data(data_nature=xbridge_utils.INVALID_DATA, char_min_size=1, char_max_size=10000)
+    
+    @unittest.skip("IN TESTING")
+    def test_valid_decode_1(self):
         try:
             """
                 [I] 2017-Oct-31 23:42:48 [0xb8] rpc call <decoderawtransaction>
@@ -22,14 +23,9 @@ class decodeUnitTest(unittest.TestCase):
                 [I] 2017-Oct-31 23:42:48 [0xb8] HTTP: resp 200 {"result":{"txid":"2e2acb1b085c3b3c5f900153a3280be69a3727b44134342c8a67b0288430f8d6","version":1,"time":1509510785,"locktime":0,"vin":[{"txid":"144981658b80cb9a1d8cd7ea48042a1612a59bd7a6ab0879f962ebaf4037a008","vout":0,"scriptSig":{"asm":"30440220111cbcdb9ccdc0e17b65e7fa1e4ec265bf76dcba8202b304b3e34ac833c297b6022008d37ca8ca5ed54bb5d217f5bcdb19c8159059e437a7dbe665c2455b1cfb93fc01 036fa429dbb28304585ebfdae5688bbac546339e578ac4403dcafdc7e15b4f08e1","hex":"4730440220111cbcdb9ccdc0e17b65e7fa1e4ec265bf76dcba8202b304b3e34ac833c297b6022008d37ca8ca5ed54bb5d217f5bcdb19c8159059e437a7dbe665c2455b1cfb93fc0121036fa429dbb28304585ebfdae5688bbac546339e578ac4403dcafdc7e15b4f08e1"},"sequence":4294967295}],"vout":[{"value":2.01000000,"n":0,"scriptPubKey":{"asm":"OP_HASH160 5abd1ae96433936b627d249c0c1a577704563235 OP_EQUAL","hex":"a9145abd1ae96433936b627d249c0c1a57770456323587","reqSigs":1,"type":"scripthash","addresses":["bM143kSgrZsEksuk2QkiU7zCdGFffyx7iG"]}},{"value":47.95000000,"n":1,"scriptPubKey":{"asm":"OP_DUP OP_HASH160 a5a4651accd909fccaa66e34a7fb5c275c6270ef OP_EQUALVERIFY OP_CHECKSIG","hex":"76a914a5a4651accd909fccaa66e34a7fb5c275c6270ef88ac","reqSigs":1,"type":"pubkeyhash","addresses":["SD4EUkDd5kyiGByjYbGtUPrvHqFjv5nVnZ"]}}]},"error":null,"id":1}
             """
             self.assertIsInstance(xbridge_rpc.decode_raw_tx("01000000814ef9590108a03740afeb62f97908aba6d79ba512162a0448ead78c1d9acb808b65814914000000006a4730440220111cbcdb9ccdc0e17b65e7fa1e4ec265bf76dcba8202b304b3e34ac833c297b6022008d37ca8ca5ed54bb5d217f5bcdb19c8159059e437a7dbe665c2455b1cfb93fc0121036fa429dbb28304585ebfdae5688bbac546339e578ac4403dcafdc7e15b4f08e1ffffffff0290ab1e000000000017a9145abd1ae96433936b627d249c0c1a57770456323587b0a8db02000000001976a914a5a4651accd909fccaa66e34a7fb5c275c6270ef88ac00000000"), dict)
-            # print("decode_raw_tx Valid Unit Test Group 1 OK")
         except AssertionError as e:
-            # print("****** decode_raw_tx valid Unit Test Group 1 FAILED ******")
-            xbridge_logger.logger.info('decode_raw_tx valid unit test group 1 FAILED')
+            xbridge_logger.logger.info('test_valid_decode_1 FAILED')
     
-    """
-            - Basic tests
-    """
     def test_invalid_decode_1(self):
         for basic_garbage_str in xbridge_utils.set_of_invalid_parameters:
             with self.subTest(basic_garbage_str=basic_garbage_str):
@@ -45,11 +41,14 @@ class decodeUnitTest(unittest.TestCase):
                     log_json = {"group": "test_invalid_decode_1", "success": 0, "failure": 1, "error": 0}
                     xbridge_utils.ERROR_LOG.append(log_json)
                     xbridge_logger.logger.info('basic_garbage_str: %s \n' % basic_garbage_str)
+                    xbridge_logger.logger.info('param: %s' % basic_garbage_str)
                 except JSONRPCException as json_excpt:
                     log_json = {"group": "test_invalid_decode_1", "success": 0,  "failure": 0, "error": 1}
                     xbridge_utils.ERROR_LOG.append(log_json)
                     xbridge_logger.logger.info('test_invalid_decode_1 unit test ERROR: %s' % str(json_excpt))
+                    xbridge_logger.logger.info('param: %s' % basic_garbage_str)
 
+    
     """
           - We test various random inputs from individual character classes.
           - We then combine those character classes.
@@ -66,15 +65,16 @@ class decodeUnitTest(unittest.TestCase):
                         self.assertIsInstance(xbridge_rpc.decode_raw_tx(generated_str), dict)
                         log_json = {"group": "test_invalid_decode_2", "success": 1, "failure": 0, "error": 0}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                    except AssertionError as e:
-                        xbridge_logger.logger.info('decode_raw_tx unit test group 2 FAILED on parameter: %s', generated_str)
+                    except AssertionError as ass_err:
+                        xbridge_logger.logger.info('test_invalid_decode_2 FAILED: %s', ass_err)
                         log_json = {"group": "test_invalid_decode_2", "success": 0, "failure": 1, "error": 0}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                        xbridge_logger.logger.info('basic_garbage_str: %s \n' % generated_str)
+                        xbridge_logger.logger.info('param: %s \n' % generated_str)
                     except JSONRPCException as json_excpt:
                         log_json = {"group": "test_invalid_decode_2", "success": 0, "failure": 0, "error": 1}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                        xbridge_logger.logger.info('test_invalid_decode_2 unit test ERROR: %s' % str(json_excpt))
+                        xbridge_logger.logger.info('test_invalid_decode_2 ERROR: %s' % str(json_excpt))
+                        xbridge_logger.logger.info('param: %s \n' % generated_str)
 
 
     """
@@ -92,15 +92,16 @@ class decodeUnitTest(unittest.TestCase):
                         self.assertIsInstance(xbridge_rpc.decode_raw_tx(generated_str), dict)
                         log_json = {"group": "test_invalid_decode_3", "success": 1, "failure": 0, "error": 0}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                    except AssertionError as e:
-                        xbridge_logger.logger.info('decode_raw_tx unit test group 3 FAILED on parameter: %s', generated_str)
+                    except AssertionError as ass_err:
+                        xbridge_logger.logger.info('test_invalid_decode_3 FAILED: %s', ass_err)
                         log_json = {"group": "test_invalid_decode_3", "success": 0, "failure": 1, "error": 0}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                        xbridge_logger.logger.info('basic_garbage_str: %s \n' % generated_str)
+                        xbridge_logger.logger.info('param: %s \n' % generated_str)
                     except JSONRPCException as json_excpt:
                         log_json = {"group": "test_invalid_decode_3", "success": 0, "failure": 0, "error": 1}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                        xbridge_logger.logger.info('test_invalid_decode_3 unit test ERROR: %s' % str(json_excpt))
+                        xbridge_logger.logger.info('test_invalid_decode_3 ERROR: %s' % str(json_excpt))
+                        xbridge_logger.logger.info('param: %s \n' % generated_str)
 
                             
     """
@@ -119,15 +120,16 @@ class decodeUnitTest(unittest.TestCase):
                         self.assertIsInstance(xbridge_rpc.decode_raw_tx(generated_str), dict)
                         log_json = {"group": "test_invalid_decode_4", "success": 1, "failure": 0, "error": 0}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                    except AssertionError as e:
-                        xbridge_logger.logger.info('decode_raw_tx unit test group 4 FAILED on parameter: %s', generated_str)
+                    except AssertionError as ass_err:
+                        xbridge_logger.logger.info('test_invalid_decode_4 FAILED: %s', ass_err)
                         log_json = {"group": "test_invalid_decode_4", "success": 0, "failure": 1, "error": 0}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                        xbridge_logger.logger.info('basic_garbage_str: %s \n' % generated_str)
+                        xbridge_logger.logger.info('param: %s \n' % generated_str)
                     except JSONRPCException as json_excpt:
                         log_json = {"group": "test_invalid_decode_4", "success": 0, "failure": 0, "error": 1}
                         xbridge_utils.ERROR_LOG.append(log_json)
-                        xbridge_logger.logger.info('test_invalid_decode_4 unit test ERROR: %s' % str(json_excpt))
+                        xbridge_logger.logger.info('test_invalid_decode_4 ERROR: %s' % str(json_excpt))
+                        xbridge_logger.logger.info('param: %s \n' % generated_str)
 
 
 """
@@ -144,3 +146,4 @@ if __name__ == '__main__':
 
 unittest.main()
 """
+
