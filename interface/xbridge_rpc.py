ea8736ab2a609599e1ff3810b5c924fc252e2f8a
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-11-21 19:50:57 +0100

    Add files via upload

--- interface/xbridge_rpc.py
+++ interface/xbridge_rpc.py
@@ -44,6 +44,22 @@ def get_node_list():
 def get_tx(txid):
     return rpc_connection.getrawtransaction(txid)
 
+# sendtoaddress "blocknetdxaddress" amount ( "comment" "comment-to" )
+def sendtoaddress(blocknetdxaddress=None, amount=None, comment=None, commentto=None):
+    try:
+        return rpc_connection.sendtoaddress(blocknetdxaddress, amount, comment, commentto)
+    except JSONRPCException as json_excpt:
+        if "get_value" in str(json_excpt) and "called on" in str(json_excpt):
+            raise xbridge_custom_exceptions.ValidBlockNetException from json_excpt
+        if "Invalid BlocknetDX address" in str(json_excpt):
+            raise xbridge_custom_exceptions.ValidBlockNetException from json_excpt
+        if "Please enter the wallet passphrase with walletpassphrase first" in str(json_excpt):
+            raise xbridge_custom_exceptions.ValidBlockNetException from json_excpt
+        if "Insufficient funds" in str(json_excpt):
+            raise xbridge_custom_exceptions.ValidBlockNetException from json_excpt
+        if "Invalid amount" in str(json_excpt):
+            raise xbridge_custom_exceptions.ValidBlockNetException from json_excpt
+
 def walletpassphrasechange(old=None, new=None):
     try:
         return rpc_connection.walletpassphrasechange(old, new)
@@ -315,190 +331,3 @@ def spork(name_param=None, value_param=None):
     except JSONRPCException as json_err:
         raise xbridge_custom_exceptions.ValidBlockNetException("JSONRPCException") from json_err
 
-
-
-"""
-rst = get_tx_info("240c472714c1ff14e5f66a6c93ae6f0efb2f4eff593ae31435e829126a0006cc")
-print(rst)
-print(len(rst))
-
-rst = cancel_tx("240c472714c1ff14e5f66a6c93ae6f0efb2f4eff593ae31435e829126a0006cc")
-print(rst)
-
-rst = accept_tx("240c472714c1ff14e5f66a6c93ae6f0efb2f4eff593ae31435e829126a0006cc", "sdksdks", "sdksldk")
-print(rst)
-
-rst = create_tx("d6a0006cc", "sdksdks", 1, "sdksldk", "sdksldk", 2)
-print(rst)
-
-"""
-
-"""
-print(get_blockcount())
-print(get_budget())
-node_list = get_node_list()
-print(len(node_list))
-print(get_core_version())
-"""
-
-"""
-valid_src_Address = "edC5dCD9BEC36A2Baa2C5b15Bfc88b93f1cF2A3D838836c67aDaEb6"
-valid_dest_Address = "C195DbfCcDb1F05f50d6E7919f19dE6E7ed"
-valid_src_Token = "ZCSJH"
-valid_dest_Token = "BSOUR"
-
-invalid_sm_positive_nb = 0.0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
-invalid_lg_positive_nb = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
-
-
-# def create_tx(fromAddress, fromToken, fromAmount, toAddress, toToken, toAmount):
-print(create_tx(valid_src_Address, valid_src_Token, invalid_lg_positive_nb, valid_dest_Address, valid_dest_Token, invalid_lg_positive_nb))
-print(create_tx(valid_src_Address, valid_src_Token, 2, valid_dest_Address, valid_dest_Token, 1000))
-print(create_tx(valid_src_Address, valid_src_Token, "2,2", valid_dest_Address, valid_dest_Token, 1000))
-print(create_tx(valid_src_Address, valid_src_Token, "2,2", valid_dest_Address, valid_dest_Token, invalid_lg_positive_nb))
-
-print(rpc_connection.settxfee(0))
-# return list
-print(rpc_connection.getaddressesbyaccount("SyscoinTest"))
-print(rpc_connection.getaddressesbyaccount(""))
-
-returns dict
-print(rpc_connection.getnettotals())
-print(rpc_connection.getnetworkinfo())
-
-print(rpc_connection.ping())
-print(rpc_connection.getpeerinfo())
-
-# print(rpc_connection.getnewaddress())
-# print(rpc_connection.getaccount("dsdsd"))
-# print(rpc_connection.getaccountaddress("dsds"))
-print(rpc_connection.getaddressesbyaccount("cwxcxwc"))
-
-print(rpc_connection.decodescript(""))
-
-# print(rpc_connection.decodescript("sdsqdqsdsq"))
-# print(rpc_connection.decoderawtransaction(""))
-# print(rpc_connection.decoderawtransaction("sdsqdqsdsq"))
-# print(rpc_connection.settxfee(-3))
-# from logs
-
-
-print(rpc_connection.getbalance())
-print(rpc_connection.getbalance("sdsdsd"))
-print(rpc_connection.getblockhash(100))
-
-print(type(rpc_connection.getreceivedbyaccount("dsds")))
-
-print(send_tx(""))
-
-print(rpc_connection.submitblock(" "))
-print(type(rpc_connection.submitblock("dsds")))
-
-print(rpc_connection.getdifficulty())
-print(type(rpc_connection.getdifficulty()))
-
-returns None
-print(rpc_connection.keypoolrefill())
-"""
-
-# print(rpc_connection.setaccount(rpc_connection.getnewaddress(), "dsqdsqdsqdsqdsqdsqd"))
-# print(rpc_connection.listreceivedbyaccount(rpc_connection.getnewaddress()))
-# print(rpc_connection.listreceivedbyaccount(99, ""))
-
-# print(rpc_connection.listreceivedbyaddress("dsds"))
-
-# ConnectionAbortedError: [WinError 10053] Une connexion établie a été abandonnée par un logiciel de votre ordinateur hôte
-# print(rpc_connection.encryptwallet("dsds"))
-
-# Invalid BlocknetDX address
-# print(rpc_connection.dumpprivkey("dsds"))
-
-# print(rpc_connection.dumpprivkey("dsds"))
-
-# JSONRPCException: -8: Cannot open wallet dump file
-# print(rpc_connection.dumpwallet("dsds"))
-
-# Auto Combine Rewards Threshold Set
-# print(rpc_connection.autocombinerewards(True, -99999999999999))
-# print(rpc_connection.autocombinerewards(False, -99999999999999))
-# print(rpc_connection.autocombinerewards(False))
-# Error
-# print(rpc_connection.autocombinerewards(True))
-
-# List
-# print(rpc_connection.multisend("print"))
-# Error with list of commands
-# print(rpc_connection.multisend(""))
-
-# print(get_tx_info("c9a59af05356605a9c028ea7c0b9f535393d9ffe32cda4af23e3c9ccc0e5f64a"))
-
-# print(rpc_connection.lockunspent())
-
-"""
-bitcoinrpc.authproxy.JSONRPCException: -1: spork <name> [<value>]
-<name> is the corresponding spork name, or 'show' to show all current spork settings, active to show which sporks are active<value> is a epoch datetime to enable or disable spork
-Requires wallet passphrase to be set with walletpassphrase call.
-"""
-# print(rpc_connection.spork())
-
-# bitcoinrpc.authproxy.JSONRPCException: -1: get_value< string > called on integer Value
-# print(rpc_connection.mnsync(3))
-
-# print(rpc_connection.encryptwallet("mypwd"))
-
-# addr = rpc_connection.getnewaddress()
-# print(rpc_connection.bip38decrypt(addr))
-
-# print(rpc_connection.getrawmempool(-1))
-
-# print(decode_raw_tx(9999999999999999999999999999999999999999999999999999999999999999))
-# print(decode_raw_tx(-9999999999999999999999999999999999999999999999999999999999999999))
-
-# print(send_tx("-"))
-# print(send_tx(9999999999999999999999999999999999999999999999999999999999999999))
-# print(send_tx(-9999999999999999999999999999999999999999999999999999999999999999))
-
-# print(rpc_connection.getmininginfo())
-# print(rpc_connection.getnetworkhashps())
-# print(rpc_connection.getnetworkhashps(0, -9999999))
-# print(rpc_connection.prioritisetransaction(9999999999999999999999999999999999999999999999999999999999999999, "-", " "))
-
-# print(importprivkey(True))
-# bitcoinrpc.authproxy.JSONRPCException: -3: Expected type integer, got real
-# bitcoinrpc.authproxy.JSONRPCException: -3: Expected type integer, got string
-# decimal
-# print(rpc_connection.estimatefee(0))
-# print(rpc_connection.estimatefee(-100))
-
-# returns None if wrong int or bool, or if wrong string {'isvalid': False}
-# print(verifymessage("dsds", "dsd", "sdsdk"))
-# print(rpc_connection.importprivkey("dsdsdddddddddddddddddddddddddd"))
-# print(rpc_connection.reservebalance(True, "dklsd"))
-
-# print(rpc_connection.getaddednodeinfo(False, ""))
-# print(addnode("dsd", "dsdsd"))
-
-# print(rpc_connection.getinfo())
-
-## print(getaddednodeinfo("dsd", "dsd"))
-
-# {'id': '0000000000000000000000000000000000000000000000000000000000000000'}
-# print(create_tx("", "", 0, "", "", 0))
-# print(create_tx("dsds", "dsd", 10, "dsqdsq", "dsqdsqd", 10))
-
-# returns a list
-# print(rpc_connection.listtransactions("", 0, 0, False))
-# print(rpc_connection.listtransactions("", 0, 0, True))
-
-# print(rpc_connection.bip38decrypt(-2))
-# print(rpc_connection.validateaddress("dksldks"))
-# print(rpc_connection.validateaddress(2))
-# print(rpc_connection.verifymessage(rpc_connection.getnewaddress(), "dsdsd", "dsdsds"))
-
-# print(rpc_connection.encryptwallet("mypwd"))
-# print(rpc_connection.walletpassphrase("mypwd", 60, False))
-# print(rpc_connection.dumpwallet("C:\\Users\\kbentahmed\\Desktop\\BlockNetDX\\test_wallet_dump5.dat"))
-
-# None if successful
-# print(rpc_connection.walletpassphrasechange("mypwd2", "mypwd"))
-# print(rpc_connection.walletlock(""))
