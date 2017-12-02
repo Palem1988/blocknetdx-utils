05ee89af0815d087f5950e4e00c03434820bdd0e
infinity7592 <33160323+infinity7592@users.noreply.github.com>
2017-12-02 16:47:34 +0100

    Add files via upload

--- rpc_test_cases/xbridge_rpc_wallet_file_test.py
+++ rpc_test_cases/xbridge_rpc_wallet_file_test.py
@@ -18,7 +18,7 @@ class wallet_File_UnitTest(unittest.TestCase):
     def test_importwallet_invalid(self):
         if xbridge_config.get_wallet_decryption_passphrase() == "":
             return
-        print("wallet passphrase: " + xbridge_config.get_wallet_decryption_passphrase())
+        # print("wallet passphrase: " + xbridge_config.get_wallet_decryption_passphrase())
         valid_passphrase = xbridge_config.get_wallet_decryption_passphrase()
         random_int = xbridge_utils.generate_random_int(-999999999999, 999999999999)
         random_bool = random.choice([True, False])
@@ -27,22 +27,16 @@ class wallet_File_UnitTest(unittest.TestCase):
             with self.subTest(basic_garbage_str=basic_garbage_str):
                 try:
                     self.assertRaises(xbridge_custom_exceptions.ValidBlockNetException, xbridge_rpc.importwallet, basic_garbage_str)
-                    log_json = {"group": "test_importwallet_invalid", "success": 1, "failure": 0, "error": 0}
-                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.XLOG("test_importwallet_invalid", 0)
                 except AssertionError as ass_err:
-                    log_json = {"group": "test_importwallet_invalid", "success": 0, "failure": 1, "error": 0}
-                    xbridge_utils.ERROR_LOG.append(log_json)
-                    xbridge_logger.logger.info('test_importwallet_invalid FAILED: %s' % ass_err)
-                    xbridge_logger.logger.info('param: %s \n' % basic_garbage_str)
+                    xbridge_logger.XLOG("test_importwallet_invalid", 1, ass_err, [basic_garbage_str])
                 except JSONRPCException as json_excpt:
-                    log_json = {"group": "test_importwallet_invalid", "success": 0,  "failure": 0, "error": 1}
-                    xbridge_utils.ERROR_LOG.append(log_json)
-                    xbridge_logger.logger.info('test_importwallet_invalid ERROR: %s' % str(json_excpt))
+                    xbridge_logger.XLOG("test_importwallet_invalid", 2, json_excpt, [basic_garbage_str])
 
     def test_backupwallet_invalid(self):
         if xbridge_config.get_wallet_decryption_passphrase() == "":
             return
-        print("wallet passphrase: " + xbridge_config.get_wallet_decryption_passphrase())
+        # print("wallet passphrase: " + xbridge_config.get_wallet_decryption_passphrase())
         valid_passphrase = xbridge_config.get_wallet_decryption_passphrase()
         random_int = xbridge_utils.generate_random_int(-999999999999, 999999999999)
         random_bool = random.choice([True, False])
@@ -51,16 +45,10 @@ class wallet_File_UnitTest(unittest.TestCase):
             with self.subTest(basic_garbage_str=basic_garbage_str):
                 try:
                     self.assertIsNone(xbridge_rpc.rpc_connection.backupwallet, basic_garbage_str)
-                    log_json = {"group": "test_backupwallet_invalid", "success": 1, "failure": 0, "error": 0}
-                    xbridge_utils.ERROR_LOG.append(log_json)
+                    xbridge_logger.XLOG("test_backupwallet_invalid", 0)
                 except AssertionError as ass_err:
-                    log_json = {"group": "test_backupwallet_invalid", "success": 0, "failure": 1, "error": 0}
-                    xbridge_utils.ERROR_LOG.append(log_json)
-                    xbridge_logger.logger.info('test_backupwallet_invalid FAILED: %s' % ass_err)
-                    xbridge_logger.logger.info('param: %s \n' % basic_garbage_str)
+                    xbridge_logger.XLOG("test_backupwallet_invalid", 1, ass_err, [basic_garbage_str])
                 except JSONRPCException as json_excpt:
-                    log_json = {"group": "test_backupwallet_invalid", "success": 0,  "failure": 0, "error": 1}
-                    xbridge_utils.ERROR_LOG.append(log_json)
-                    xbridge_logger.logger.info('test_backupwallet_invalid ERROR: %s' % str(json_excpt))
+                    xbridge_logger.XLOG("test_backupwallet_invalid", 2, json_excpt, [basic_garbage_str])
 
 # unittest.main()
